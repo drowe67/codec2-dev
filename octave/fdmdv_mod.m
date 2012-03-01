4bee044931bd764ae7747afb684be2fcc694f75b
drowe67 <drowe67@01035d8c-6547-0410-b346-abe4f91aad63>
2012-03-01 00:58:38 +0000

    moved modem functions into a library script file and wrote _ut and _mod functions, progressing to wave file I/O

---
+++ octave/fdmdv_mod.m
@@ -0,0 +1,29 @@
+% fdmdv_mod.m
+%
+% Modulator function for FDMDV modem.
+%
+% Copyright David Rowe 2012
+% This program is distributed under the terms of the GNU General Public License 
+% Version 2
+%
+
+function tx_fdm = fdmdv_mod(wavefilename, nbits)
+
+% Main loop ----------------------------------------------------
+
+fdmdv; % include modem code
+
+frames = floor(nbits/(Nc*Nb));
+tx_fdm = [];
+tx_gain = 1000;
+
+for i=1:frames
+  tx_bits = get_test_bits(Nc*Nb);
+  tx_symbols = bits_to_qpsk(tx_bits);
+  tx_baseband = tx_filter(tx_symbols);
+  tx_fdm = [tx_fdm fdm_upconvert(tx_baseband)];
+end
+
+tx_fdm *= tx_gain;
+
+wavwrite(tx_fdm',8000,16,wavefilename)
