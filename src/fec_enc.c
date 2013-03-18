55f220db6503895bb9178f38c08a8783afe8140b
drowe67 <drowe67@01035d8c-6547-0410-b346-abe4f91aad63>
2013-03-18 04:23:58 +0000

    new fec modes and interleaver for 1600

--- src/fec_enc.c
+++ src/fec_enc.c
@@ -38,6 +38,10 @@
 #include <string.h>
 #include <errno.h>
 
+#define MODE_1600 0
+#define MODE_1850 1
+#define MODE_2000 2
+
 int main(int argc, char *argv[])
 {
     void          *codec2, *fdmdv;
@@ -49,12 +53,12 @@ int main(int argc, char *argv[])
     int            bits_per_output_frame, bytes_per_output_frame;
     unsigned char *packed_output_bits;
     int           *unpacked_output_bits;
-    int            mode, Nc, bit, byte;
+    int            codec2_mode, mode, Nc, bit, byte;
     int            i,j;
     int            data, codeword1, codeword2;
 
     if (argc < 3) {
-	printf("%s InputFromCodecFile OutputToModemWithFECFile\n", argv[0]);
+	printf("%s InputFromCodecFile OutputToModemWithFECFile [2000|1850|1600]\n", argv[0]);
 	exit(1);
     }
 
@@ -72,14 +76,34 @@ int main(int argc, char *argv[])
 	exit(1);
     }
 
+    if ((argc != 4) || (strcmp(argv[3],"2000") == 0)) {
+        /* 2000 bit/s with FEC */
+        mode = MODE_2000;
+	codec2_mode = CODEC2_MODE_1400;
+        Nc = 20;
+    } else if ((strcmp(argv[3],"1850") == 0)) {
+        /* 1850 bit/s with FEC */
+        mode = MODE_1850;
+	codec2_mode = CODEC2_MODE_1300;
+        Nc = 20;
+    }
+    else if (strcmp(argv[3],"1600") == 0) {
+        /* 1600 bit/s with FEC (actually 1575 with one spare) */
+        mode = MODE_1600;
+	codec2_mode = CODEC2_MODE_1300;
+        Nc = 16;
+    }
+    else {
+	fprintf(stderr, "Error in mode: %s.  Must be 2000, 1850, or 1600\n", argv[3]);
+	exit(1);
+    }
+    
     /* input parameters and buffers */
 
-    mode = CODEC2_MODE_1400;
-    codec2 = codec2_create(mode);
+    codec2 = codec2_create(codec2_mode);
 
     bits_per_input_frame = codec2_bits_per_frame(codec2);
-    bytes_per_input_frame = bits_per_input_frame / 8;
-    assert((bits_per_input_frame % 8) == 0); /* make sure integer number of bytes per frame */
+    bytes_per_input_frame = (bits_per_input_frame + 7)/8;
 
     packed_input_bits = (unsigned char*)malloc(bytes_per_input_frame*sizeof(char));
     assert(packed_input_bits != NULL);
@@ -92,20 +116,19 @@ int main(int argc, char *argv[])
        bother us here, as fdmdv_mod takes care of that.
     */
 
-    Nc = 20;
     fdmdv = fdmdv_create(Nc);
 
     bits_per_output_frame = 2*fdmdv_bits_per_frame(fdmdv);
-    bytes_per_output_frame = bits_per_output_frame/8;
-    assert((bits_per_output_frame % 8) == 0); /* make sure integer number of bytes per frame */
+    bytes_per_output_frame = (bits_per_output_frame+7)/8;
 
     packed_output_bits = (unsigned char*)malloc(bytes_per_output_frame*sizeof(char));
     assert(packed_output_bits != NULL);
     unpacked_output_bits = (int*)malloc(bits_per_output_frame*sizeof(int));
     assert(unpacked_output_bits != NULL);
     
-    //fprintf(stderr, "input bits: %d  input_bytes: %d  output_bits: %d  output_bytes: %d\n",
-    //        bits_per_input_frame,  bytes_per_input_frame, bits_per_output_frame,  bytes_per_output_frame);
+    fprintf(stderr, "mode: %d  Nc: %d\n", mode, Nc);
+    fprintf(stderr, "input bits: %d  input_bytes: %d  output_bits: %d  output_bytes: %d\n",
+            bits_per_input_frame,  bytes_per_input_frame, bits_per_output_frame,  bytes_per_output_frame);
 
     /* main loop */
 
@@ -124,47 +147,126 @@ int main(int argc, char *argv[])
 		byte++;
 	    }
 	}
-	assert(byte == bytes_per_input_frame);
 
-        /* add FEC  ----------------------------------*/
+        /* add FEC  ---------------------------------------------------------*/
 
-        /* Protect first 24 bits with (23,12) Golay Code.  The first
-           24 bits are the most sensitive, as they contain the
-           pitch/energy VQ and voicing bits. This uses 56 + 11 + 11 =
-           78 bits, so we have two spare in 80 bit frame sent to
-           modem. */
+        if (mode == MODE_2000) {
+            /* Protect first 24 bits with (23,12) Golay Code.  The first
+               24 bits are the most sensitive, as they contain the
+               pitch/energy VQ and voicing bits. This uses 56 + 11 + 11 =
+               78 bits, so we have two spare in 80 bit frame sent to
+               modem. */
 
-        /* first codeword */
+            /* first codeword */
 
-        data = 0;
-        for(i=0; i<12; i++) {
-            data <<= 1;
-            data |= unpacked_input_bits[i];
+            data = 0;
+            for(i=0; i<12; i++) {
+                data <<= 1;
+                data |= unpacked_input_bits[i];
+            }
+            codeword1 = golay23_encode(data);
+            //fprintf(stderr, "data1: 0x%x codeword1: 0x%x\n", data, codeword1);
+
+            /* second codeword */
+
+            data = 0;
+            for(i=12; i<24; i++) {
+                data <<= 1;
+                data |= unpacked_input_bits[i];
+            }
+            codeword2 = golay23_encode(data);
+            //fprintf(stderr, "data: 0x%x codeword2: 0x%x\n", data, codeword2);
+
+            /* now pack output frame with parity bits at end to make them
+               as far apart as possible from the data the protect.  Parity
+               bits are LSB of the Golay codeword */
+
+            for(i=0; i<bits_per_input_frame; i++)
+                unpacked_output_bits[i] = unpacked_input_bits[i];
+            for(j=0; i<bits_per_input_frame+11; i++,j++) {
+                unpacked_output_bits[i] = (codeword1 >> (10-j)) & 0x1;
+            }
+            for(j=0; i<bits_per_input_frame+11+11; i++,j++) {
+                unpacked_output_bits[i] = (codeword2 >> (10-j)) & 0x1;
+            }
         }
-        codeword1 = golay23_encode(data);
-        //fprintf(stderr, "data1: 0x%x codeword1: 0x%x\n", data, codeword1);
 
-        /* second codeword */
+        if (mode == MODE_1850) {
 
-        data = 0;
-        for(i=12; i<24; i++) {
-            data <<= 1;
-            data |= unpacked_input_bits[i];
+            /* Protect first 12 out of first 16 excitation bits with (23,12) Golay Code:
+
+               0,1,2,3: v[0]..v[1]
+               4,5,6,7: MSB of pitch
+               11,12,13,14: MSB of energy
+
+            */
+
+            data = 0;
+            for(i=0; i<8; i++) {
+                data <<= 1;
+                data |= unpacked_input_bits[i];
+            }
+            for(i=11; i<15; i++) {
+                data <<= 1;
+                data |= unpacked_input_bits[i];
+            }
+            codeword1 = golay23_encode(data);
+
+            /* Protect first 12 LSP bits with (23,12) Golay Code */
+
+            data = 0;
+            for(i=16; i<28; i++) {
+                data <<= 1;
+                data |= unpacked_input_bits[i];
+            }
+            codeword2 = golay23_encode(data);
+            fprintf(stderr, "codeword2: 0x0%x\n", codeword2);
+            
+            /* now pack output frame with parity bits at end to make them
+               as far apart as possible from the data they protect.  Parity
+               bits are LSB of the Golay codeword */
+
+            for(i=0; i<bits_per_input_frame; i++)
+                unpacked_output_bits[i] = unpacked_input_bits[i];
+            for(j=0; i<bits_per_input_frame+11; i++,j++) {
+                unpacked_output_bits[i] = (codeword1 >> (10-j)) & 0x1;
+                unpacked_output_bits[i+11] = (codeword2 >> (10-j)) & 0x1;
+            }
+            for(i=bits_per_input_frame+11+11; i<bits_per_output_frame; i++)
+                unpacked_output_bits[i] = 0;
         }
-        codeword2 = golay23_encode(data);
-        //fprintf(stderr, "data: 0x%x codeword2: 0x%x\n", data, codeword2);
 
-        /* now pack output frame with parity bits at end to make them
-           as far apart as possible from the data the protect.  Parity
-           bits are LSB of the Golay codeword */
+        if (mode == MODE_1600) {
 
-        for(i=0; i<bits_per_input_frame; i++)
-            unpacked_output_bits[i] = unpacked_input_bits[i];
-        for(j=0; i<bits_per_input_frame+11; i++,j++) {
-            unpacked_output_bits[i] = (codeword1 >> (10-j)) & 0x1;
-        }
-        for(j=0; i<bits_per_input_frame+11+11; i++,j++) {
-            unpacked_output_bits[i] = (codeword2 >> (10-j)) & 0x1;
+            /* Protect first 12 out of first 16 excitation bits with (23,12) Golay Code:
+
+               0,1,2,3: v[0]..v[1]
+               4,5,6,7: MSB of pitch
+               11,12,13,14: MSB of energy
+
+            */
+
+
+            data = 0;
+            for(i=0; i<8; i++) {
+                data <<= 1;
+                data |= unpacked_input_bits[i];
+            }
+            for(i=11; i<15; i++) {
+                data <<= 1;
+                data |= unpacked_input_bits[i];
+            }
+            codeword1 = golay23_encode(data);
+
+            /* now pack output frame with parity bits at end to make them
+               as far apart as possible from the data they protect.  Parity
+               bits are LSB of the Golay codeword */
+
+            for(i=0; i<bits_per_input_frame; i++)
+                unpacked_output_bits[i] = unpacked_input_bits[i];
+            for(j=0; i<bits_per_input_frame+11; i++,j++) {
+                unpacked_output_bits[i] = (codeword1 >> (10-j)) & 0x1;
+            }
         }
 
         /* pack bits, MSB first  */
