88e5e3fad24ed3ae9e1a7f8b4a93530a86b02973
drowe67 <drowe67@01035d8c-6547-0410-b346-abe4f91aad63>
2014-01-30 04:28:56 +0000

    ldpc simulation working, but perf not that great in ccir poor 4dB channel

--- codec2-dev/octave/pl2.m
+++ codec2-dev/octave/pl2.m
@@ -2,53 +2,40 @@
 % This program is distributed under the terms of the GNU General Public License 
 % Version 2
 
-function pl2(samname1, samname2, start_sam, end_sam, pngname)
+function pl2(samname1, samname2, start_sam, end_sam, offset)
   
   fs1=fopen(samname1,"rb");
   s1=fread(fs1,Inf,"short");
   fs2=fopen(samname2,"rb");
   s2=fread(fs2,Inf,"short");
 
-  st = 1;
-  en = length(s1);
+  st1 = st2 = 1;
+  en1 = en2 = length(s1);
   if (nargin >= 3)
-    st = start_sam;
+    st1 = st2 = start_sam;
   endif
   if (nargin >= 4)
-    en = end_sam;
+    en1 = en2 = end_sam;
+  endif
+
+  if (nargin == 5)
+    st2 += offset        
+    en2 += offset        
   endif
 
   figure(1);
   clf;
   subplot(211);
   l1 = strcat("r;",samname1,";");
-  plot(s1(st:en), l1);
-  axis([1 en-st min(s1(st:en)) max(s1(st:en))]);
+  plot(s1(st1:en1), l1);
+  axis([1 en1-st1 min(s1(st1:en1)) max(s1(st1:en1))]);
   subplot(212);
   l2 = strcat("r;",samname2,";");
-  plot(s2(st:en),l2);
-  axis([1 en-st min(s1(st:en)) max(s1(st:en))]);
+  plot(s2(st2:en2),l2);
+  axis([1 en2-st2 min(s1(st2:en2)) max(s1(st2:en2))]);
  
   figure(2)
-  plot(s1(st:en)-s2(st:en));
-  max(s1(st:en)-s2(st:en))
+  plot(s1(st1:en1)-s2(st2:en2));
   
-  if (nargin == 5)
-
-    % small image
-
-    __gnuplot_set__ terminal png size 420,300
-    s = sprintf("__gnuplot_set__ output \"%s.png\"", pngname);
-    eval(s)
-    replot;
-
-    % larger image
-
-    __gnuplot_set__ terminal png size 800,600
-    s = sprintf("__gnuplot_set__ output \"%s_large.png\"", pngname);
-    eval(s)
-    replot;
-
-  endif
 
 endfunction
