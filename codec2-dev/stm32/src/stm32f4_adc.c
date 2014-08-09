c72cf0d885accf8f61968c37b4862c1dfabb1be9
drowe67 <drowe67@01035d8c-6547-0410-b346-abe4f91aad63>
2014-08-09 03:39:39 +0000

    single channel driver OK on Discovery

--- codec2-dev/stm32/src/stm32f4_adc.c
+++ codec2-dev/stm32/src/stm32f4_adc.c
@@ -135,7 +135,7 @@ void adc_configure(){
 
     // Select the channel to be read from
 
-    ADC_RegularChannelConfig(ADCx,ADC_Channel_2,1,ADC_SampleTime_144Cycles);
+    ADC_RegularChannelConfig(ADCx,ADC_Channel_1,1,ADC_SampleTime_144Cycles);
 
     /* DMA  configuration **************************************/
 
