7d4237b09d857c78b56788e07cf8a6e2664bfdf2
drowe67 <drowe67@01035d8c-6547-0410-b346-abe4f91aad63>
2015-03-14 06:40:03 +0000

    coh psk demod phase correction starting to do something sensible, still to complete testing

--- octave/tcohpsk.m
+++ octave/tcohpsk.m
@@ -20,11 +20,30 @@ framesize = 160;
 load ../build_linux/unittest/tcohpsk_out.txt
 
 sim_in = standard_init();
+sim_in.framesize = 160;
+sim_in.ldpc_code = 0;
+sim_in.ldpc_code_rate = 1;
+sim_in.Nc               = 4;
+sim_in.Rs               = 50;
+sim_in.Ns               = 4;
+sim_in.Np               = 2;
+sim_in.Nchip            = 1;
+sim_in.modulation       = 'qpsk';
+sim_in.do_write_pilot_file = 0;
+sim_in = symbol_rate_init(sim_in);
+
+rand('state',1); 
 tx_bits = round(rand(1,framesize));
 tx_bits_log = [];
+tx_symb_log = [];
 for i=1:frames
   tx_bits_log = [tx_bits_log tx_bits];
+  [tx_symb tx_bits prev_tx_sym] = bits_to_qpsk_symbols(sim_in, tx_bits, [], []);
+  tx_symb_log = [tx_symb_log; tx_symb];
 end
 
-stem_sig_and_error(1, 211, tx_bits_log_c(1:n), tx_bits_log(1:n) - tx_bits_log_c(1:n), 'tx bits', [1 n -1.5 1.5])
+stem_sig_and_error(1, 111, tx_bits_log_c(1:n), tx_bits_log(1:n) - tx_bits_log_c(1:n), 'tx bits', [1 n -1.5 1.5])
+stem_sig_and_error(2, 211, real(tx_symb_log_c(1:n)), real(tx_symb_log(1:n) - tx_symb_log_c(1:n)), 'tx symb re', [1 n -1.5 1.5])
+stem_sig_and_error(2, 212, imag(tx_symb_log_c(1:n)), imag(tx_symb_log(1:n) - tx_symb_log_c(1:n)), 'tx symb im', [1 n -1.5 1.5])
 check(tx_bits_log, tx_bits_log_c, 'tx_bits');
+check(tx_symb_log, tx_symb_log_c, 'tx_symb');
---
+++ src/comp_prim.h
@@ -0,0 +1,92 @@
+/*---------------------------------------------------------------------------*\
+                                                                             
+  FILE........: comp_prim.h
+  AUTHOR......: David Rowe                                                          
+  DATE CREATED: Marh 2015
+                                                                             
+  Complex number maths primitives.
+                                                                             
+\*---------------------------------------------------------------------------*/
+
+/*
+  Copyright (C) 2015 David Rowe
+
+  All rights reserved.
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU Lesser General Public License version 2.1, as
+  published by the Free Software Foundation.  This program is
+  distributed in the hope that it will be useful, but WITHOUT ANY
+  WARRANTY; without even the implied warranty of MERCHANTABILITY or
+  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+  License for more details.
+
+  You should have received a copy of the GNU Lesser General Public License
+  along with this program; if not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef __COMP_PRIM__
+#define __COMP_PRIM__
+
+/*---------------------------------------------------------------------------*\
+                                                                             
+                               FUNCTIONS
+
+\*---------------------------------------------------------------------------*/
+
+static COMP cneg(COMP a)
+{
+    COMP res;
+
+    res.real = -a.real;
+    res.imag = -a.imag;
+
+    return res;
+}
+
+static COMP cconj(COMP a)
+{
+    COMP res;
+
+    res.real = a.real;
+    res.imag = -a.imag;
+
+    return res;
+}
+
+static COMP cmult(COMP a, COMP b)
+{
+    COMP res;
+
+    res.real = a.real*b.real - a.imag*b.imag;
+    res.imag = a.real*b.imag + a.imag*b.real;
+
+    return res;
+}
+
+static COMP fcmult(float a, COMP b)
+{
+    COMP res;
+
+    res.real = a*b.real;
+    res.imag = a*b.imag;
+
+    return res;
+}
+
+static COMP cadd(COMP a, COMP b)
+{
+    COMP res;
+
+    res.real = a.real + b.real;
+    res.imag = a.imag + b.imag;
+
+    return res;
+}
+
+static float cabsolute(COMP a)
+{
+    return sqrtf(powf(a.real, 2.0) + powf(a.imag, 2.0));
+}
+
+#endif
