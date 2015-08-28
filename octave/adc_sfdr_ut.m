866fd16d54bb040e438a34d6463070e99fe2cb3d
drowe67 <drowe67@01035d8c-6547-0410-b346-abe4f91aad63>
2015-08-28 04:14:43 +0000

    getting sensible plots from SFDR tests

---
+++ octave/adc_sfdr_ut.m
@@ -0,0 +1,28 @@
+% adc_sfdr_ut.m
+% David Rowe Aug 2015
+%
+% Processes data collected from STM32F4 or SFDR testing of ADC
+
+s = load_raw("~/stlink/adc.raw");
+Fs = 2E6;
+N = 1024;
+num_frames = length(s)/N;
+h = hanning(N);
+XdB = zeros(N/2,1);
+
+for i=1:num_frames
+  x = s((i-1)*N+1:i*N);
+  X = fft(x .* h);
+  XdB += 20*log10(abs(X(1:N/2)));
+end
+
+XdB /= num_frames;
+XdB -= max(XdB);
+
+figure(1)
+plot((0:N/2-1)*Fs/(1000*N), XdB)
+grid
+ylabel('Amplitude dB')
+xlabel('Frequency (kHz)');
+axis([0 Fs/(2*1000) -80 0])
+
