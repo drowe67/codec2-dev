aae505fe1acaa1a0ece0db120d85ebd60e78fc2a
drowe67 <drowe67@01035d8c-6547-0410-b346-abe4f91aad63>
2015-12-01 22:23:50 +0000

    scripts for VHf PA

--- octave/s_param_rf.m
+++ octave/s_param_rf.m
@@ -2,57 +2,10 @@
 %
 % David Rowe Nov 2015
 %
-% RF small signal amplifier design, using equations from "RF Cicruit
-% Design" by Chris Bowick
+% Working for small signal VHF amplifier design using
+% S-param techniques from "RF Circuit Design" by Chris Bowick
 
-1;
-
-% Helper functions -------------------------------------------------
-
-% convert a parallel R/X to a series R/X
-
-function Zs = zp_to_zs(Zp)
-  Xp = j*imag(Zp); Rp = real(Zp);
-  Zs = Xp*Rp/(Xp+Rp);
-endfunction
-
-
-% convert a series R/X to a parallel R/X
-
-function Zp = zs_to_zp(Zs)
-  Xs = imag(Zs); Rs = real(Zs);
-  Q = Xs/Rs;       
-  Rp = (Q*Q+1)*Rs;
-  Xp = Rp/Q;
-  Zp = Rp + j*Xp;
-endfunction
-
-
-% Design a Z match network with a parallel and series reactance
-% to match between a low and high resistance.  Note Xp and Xs
-% must be implemented as opposite sign, ie one a inductor, one
-% a capacitor (your choice).
-%
-%  /--Xs--+---\
-%  |      |   |
-% Rlow   Xp  Rhigh
-%  |      |   |
-%  \------+---/
-%        
-
-function [Xs Xp] = z_match(Rlow, Rhigh)
-  assert(Rlow < Rhigh, "Rlow must be < Rhigh");
-  Q = sqrt(Rhigh/Rlow -1);
-  Xs = Q*Rlow;
-  Xp = Rhigh/Q;
-endfunction
-
-
-% Design an air core inductor, Example 1-5 "RF Circuit Design"
-
-function Nturns = design_inductor(L_uH, diameter_mm)
-  Nturns = sqrt(29*L_uH/(0.394*(diameter_mm*0.1/2)));
-endfunction
+rfdesign; % library of helped functions
 
 more off;
 
