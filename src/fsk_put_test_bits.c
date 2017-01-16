bb58faf6a307a7a82697ed14f5af00b8d531e3f6
drowe67 <drowe67@01035d8c-6547-0410-b346-abe4f91aad63>
2017-01-16 23:47:06 +0000

    modified fsk get/put test frame utils so that they can re-sync on a continuous stream, updated README examples

--- src/fsk_put_test_bits.c
+++ src/fsk_put_test_bits.c
@@ -2,9 +2,10 @@
 
   FILE........: fsk_get_test_bits.c
   AUTHOR......: Brady O'Brien
-  DATE CREATED: Januar 2016
+  DATE CREATED: January 2016
 
-  Generates a pseudorandom sequence of bits for testing of fsk_mod and fsk_demod
+  Generates a pseudorandom sequence of bits for testing of fsk_mod and
+  fsk_demod.
 
 \*---------------------------------------------------------------------------*/
 
@@ -30,67 +31,72 @@
 #include <stdio.h>
 #include <string.h>
 #include "fsk.h"
-#include "codec2_fdmdv.h"
 
-#define FSK_FRAME_SIZE 100
-#define INIT_SEQ {0,1,1,0,1,0,0,1,1,1,1,0,1,1,0,0,0,0,1,0,1,1,0,1,0,1,0,1,0,0,1,1}
-
-uint8_t init[] = INIT_SEQ;
-uint8_t finit[sizeof(init)];
-
-int find_init(uint8_t next){
-	memmove(&finit[0],&finit[1],sizeof(init)-1);
-	finit[sizeof(init)-1] = next;
-	int i, err = 0;
-	for(i = 0;i<sizeof(init); i++){
-		if(init[i]!=finit[i]) err++;
-	}
-	return err<=3;
-}
+#define TEST_FRAME_SIZE 100  /* must match fsk_get_test_bits.c */
 
 int main(int argc,char *argv[]){
-    int bitcnt,bitcorr;
+    int bitcnt,biterr,i,errs;
     FILE *fin;
-    uint8_t bitbuf,cntbit;;
-    
-    /* Seed the RNG with some constant */
-    srand(158324);
+    uint8_t *bitbuf_tx, *bitbuf_rx, abit;
     
     if(argc != 2){
         fprintf(stderr,"usage: %s InputBitsOnePerByte \n",argv[0]);
         exit(1);
     }
     
-	
-	if(strcmp(argv[1],"-")==0 || argc<2){
-		fin = stdin;
-	}else{
-		fin = fopen(argv[1],"r");
-	}
+    if(strcmp(argv[1],"-")==0 || argc<2){
+        fin = stdin;
+    }else{
+        fin = fopen(argv[1],"r");
+    }
     
     if(fin==NULL){
-        fprintf(stderr,"Couldn't open test vector files\n");
+        fprintf(stderr,"Couldn't open input file: %s\n", argv[1]);
         goto cleanup;
     }
+
+    /* allocate buffers for processing */
+    bitbuf_tx = (uint8_t*)alloca(sizeof(uint8_t)*TEST_FRAME_SIZE);
+    bitbuf_rx = (uint8_t*)alloca(sizeof(uint8_t)*TEST_FRAME_SIZE);
+    
+    /* Generate known tx frame from known seed */
+    srand(158324);
+    for(i=0; i<TEST_FRAME_SIZE; i++){
+	bitbuf_tx[i] = rand()&0x1;
+	bitbuf_rx[i] = 0;
+    }
+    
     bitcnt = 0;
-    bitcorr = 0;
+    biterr = 0;
     
-    /* Find frame start word */
-    do{
-		fread(&bitbuf,sizeof(uint8_t),1,fin);
-	}while(!find_init(bitbuf));
-	
-	
-    while(fread(&bitbuf,sizeof(uint8_t),1,fin)>0){
-		cntbit = rand()&0x1;
-		if( (cntbit>0)==(bitbuf>0))
-			bitcorr++;
-		bitcnt++;
-		if(fin == stdin)
-			fflush(fin);
-	}
-	fprintf(stderr,"FSK BER %f, bits tested %d, bit errors %d\n",1-((float)bitcorr/(float)bitcnt),bitcnt,bitcnt-bitcorr);
+    while(fread(&abit,sizeof(uint8_t),1,fin)>0){
+
+        /* update silding window of input bits */
+
+        for(i=0; i<TEST_FRAME_SIZE-1; i++) {
+            bitbuf_rx[i] = bitbuf_rx[i+1];
+        }
+        bitbuf_rx[TEST_FRAME_SIZE-1] = abit;
+
+        /* compare to know tx frame.  If they are time aligned, there
+           will be a fairly low bit error rate */
+
+        errs = 0;
+        for(i=0; i<TEST_FRAME_SIZE; i++) {
+            if (bitbuf_rx[i] != bitbuf_tx[i]) {
+                errs++;
+            }
+        }
+        if (errs < 0.1*TEST_FRAME_SIZE) {
+            /* OK, we have a valid test frame sync, so lets count errors */
+            bitcnt += TEST_FRAME_SIZE;
+            biterr += errs;
+        }
+    }
+ 
+   fprintf(stderr,"FSK BER %f, bits tested %d, bit errors %d\n",
+           ((float)biterr/(float)bitcnt),bitcnt,biterr);
     
-    cleanup:
+ cleanup:
     fclose(fin);
 }
