029e9756c67708ae31b6e6543d181a02e16d7420
okcsampson <okcsampson@01035d8c-6547-0410-b346-abe4f91aad63>
2017-06-07 19:31:33 +0000

    Add getters/setters to codec2_ofdm.h

--- codec2-dev/src/codec2_ofdm.h
+++ codec2-dev/src/codec2_ofdm.h
@@ -31,11 +31,21 @@ struct OFDM *ofdm_create(float, float, int, float, float, int, int);
 void ofdm_destroy(struct OFDM *);
 int ofdm_errno(void);
 COMP *ofdm_mod(struct OFDM *ofdm, int *);
-int *ofdm_demod(struct OFDM *ofdm, COMP *);
+int *ofdm_demod(struct OFDM *ofdm, COMP []);
 
+/* getters and setters */
+
+void set_verbose(struct OFDM *, int);
+int get_verbose(struct OFDM *);
+void set_timing_enable(struct OFDM *, bool);
+void set_foff_est_enable(struct OFDM *, bool);
+void set_phase_est_enable(struct OFDM *, bool);
+void set_foff_est_gain(struct OFDM *, float);
+void set_off_est_hz(struct OFDM *, float);
 
 #ifdef __cplusplus
 }
 #endif
 
 #endif
+
