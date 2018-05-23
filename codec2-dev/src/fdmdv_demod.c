260106f629dc16dc4f7a3a6853069df9d2e069d5
drowe67 <drowe67@01035d8c-6547-0410-b346-abe4f91aad63>
2018-05-23 22:46:46 +0000

    some GPL text changes I missed checking in

--- codec2-dev/src/fdmdv_demod.c
+++ codec2-dev/src/fdmdv_demod.c
@@ -22,7 +22,7 @@
   All rights reserved.
 
   This program is free software; you can redistribute it and/or modify
-  it under the terms of the GNU Lesser General Public License version 2, as
+  it under the terms of the GNU Lesser General Public License version 2.1, as
   published by the Free Software Foundation.  This program is
   distributed in the hope that it will be useful, but WITHOUT ANY
   WARRANTY; without even the implied warranty of MERCHANTABILITY or
