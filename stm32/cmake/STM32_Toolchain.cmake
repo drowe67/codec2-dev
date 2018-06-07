4d1e36c5873dc844c3cd80215b3ce052f10c5f14
hobbes1069 <hobbes1069@01035d8c-6547-0410-b346-abe4f91aad63>
2018-06-07 18:16:13 +0000

    Commit STM32F4 library file and other tweaks.

--- stm32/cmake/STM32_Toolchain.cmake
+++ stm32/cmake/STM32_Toolchain.cmake
@@ -7,8 +7,7 @@ SET(CMAKE_CROSSCOMPILING TRUE)
 # specify the cross compiler
 set(CMAKE_C_COMPILER arm-none-eabi-gcc)
 set(CMAKE_CXX_COMPILER arm-none-eabi-cpp)
- 
 #SET(COMMON_FLAGS "-mcpu=cortex-m3 -mthumb -mthumb-interwork -msoft-float -ffunction-sections -fdata-sections -g -fno-common -fmessage-length=0")
 #SET(CMAKE_CXX_FLAGS "${COMMON_FLAGS}  -std=gnu++0x")
 #SET(CMAKE_C_FLAGS "${CMAKE_CXX_FLAGS} -std=gnu99")
-#set(CMAKE_EXE_LINKER_FLAGS "-Wl,-gc-sections ")
+set(CMAKE_EXE_LINKER_FLAGS "-T${CMAKE_SOURCE_DIR}/stm32_flash.ld")
