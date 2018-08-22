34d66117505a0bc37ee4659d96ea437628717a6a
hobbes1069 <hobbes1069@01035d8c-6547-0410-b346-abe4f91aad63>
2018-08-22 21:30:44 +0000

    STM32 cmake updates for Ubuntu.

--- codec2-dev/stm32/cmake/STM32_Toolchain.cmake
+++ codec2-dev/stm32/cmake/STM32_Toolchain.cmake
@@ -2,12 +2,14 @@
  
 set(CMAKE_SYSTEM_NAME Generic)
 set(CMAKE_SYSTEM_PROCESSOR arm)
-#set(CMAKE_SYSTEM_VERSION 1)
-#set(CMAKE_CROSSCOMPILING TRUE)
+set(CMAKE_SYSTEM_VERSION 1)
  
 # specify the cross compiler
 set(CMAKE_C_COMPILER arm-none-eabi-gcc)
 set(CMAKE_CXX_COMPILER arm-none-eabi-cpp)
+set(CMAKE_C_FLAGS_INIT "--specs=nosys.specs")
+set(CMAKE_CXX_FLAGS_INIT "--specs=nosys.specs")
+
 
 set(CMAKE_EXE_LINKER_FLAGS "-T${CMAKE_SOURCE_DIR}/stm32_flash.ld")
 set(CMAKE_EXECUTABLE_SUFFIX_C ".elf")
