61c26734675d5be055e5dc163b7bf5910d500405
hobbes1069 <hobbes1069@01035d8c-6547-0410-b346-abe4f91aad63>
2018-08-30 12:35:44 +0000

    More CMake fixes for Ubuntu.

--- codec2-dev/stm32/cmake/STM32_Toolchain.cmake
+++ codec2-dev/stm32/cmake/STM32_Toolchain.cmake
@@ -5,12 +5,5 @@ set(CMAKE_SYSTEM_VERSION 1)
 # specify the cross compiler
 set(CMAKE_C_COMPILER arm-none-eabi-gcc)
 set(CMAKE_CXX_COMPILER arm-none-eabi-cpp)
-set(CMAKE_C_FLAGS_INIT "--specs=nosys.specs")
-set(CMAKE_CXX_FLAGS_INIT "--specs=nosys.specs")
-
-# Macro for elf->bin
-macro(elf2bin target)
-    add_custom_command(TARGET ${target}
-    POST_BUILD COMMAND ${CMAKE_OBJCOPY} -O binary ${target}.elf ${target}.bin
-    COMMENT "Creating binary for ${target}")
-endmacro()
+set(CMAKE_C_FLAGS_INIT "-specs=nosys.specs")
+set(CMAKE_CXX_FLAGS_INIT "-specs=nosys.specs")
