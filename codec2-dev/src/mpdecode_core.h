ac47e072fa68016ed36383e3cd5a1e14a293ab88
don_reid <don_reid@01035d8c-6547-0410-b346-abe4f91aad63>
2018-09-12 19:39:27 +0000

    Remove unused functions and their declarations. With this as a separate checkin, they can be restored easily if needed later.

--- codec2-dev/src/mpdecode_core.h
+++ codec2-dev/src/mpdecode_core.h
@@ -34,8 +34,8 @@ void encode(struct LDPC *ldpc, unsigned char ibits[], unsigned char pbits[]);
 
 int run_ldpc_decoder(struct LDPC *ldpc, char out_char[], double input[], int *parityCheckCount);
 
+// Used for test programs.
 void sd_to_llr(double llr[], double sd[], int n);
-
 void Demod2D(double symbol_likelihood[], COMP r[], COMP S_matrix[], float EsNo, float fading[], float mean_amp, int number_symbols);
 void Somap(double bit_likelihood[], double symbol_likelihood[], int number_symbols);
 void symbols_to_llrs(double llr[], COMP rx_qpsk_symbols[], float rx_amps[], float EsNo, float mean_amp, int nsyms);
@@ -56,49 +56,5 @@ struct c_node {
   int *socket; /* socket number at the v_node */
 };
 
-void init_c_v_nodes(struct c_node *c_nodes, 
-                    int     shift, 
-                    int     NumberParityBits, 
-                    int     max_row_weight,
-                    double *H_rows,
-                    int     H1,
-                    int     CodeLength,
-                    struct v_node *v_nodes, 
-                    int     NumberRowsHcols, 
-                    double *H_cols,
-                    int     max_col_weight,
-                    int     dec_type,
-                    double *input);
-
-void ApproximateMinStar(	 int	  BitErrors[],
-				 int      DecodedBits[],
-				 struct c_node c_nodes[],
-				 struct v_node v_nodes[],
-				 int	  CodeLength,
-				 int	  NumberParityBits,
-				 int	  max_iter );
-
-void MinSum(		 int	  BitErrors[],
-				 int      DecodedBits[],
-				 struct c_node c_nodes[],
-				 struct v_node v_nodes[],
-				 int	  CodeLength,
-				 int	  NumberParityBits,
-				 int	  max_iter, 
-				 float    r_scale_factor,
-				 float    q_scale_factor, 
-				 int      data[] );
-
-
-void SumProduct(	 int	  BitErrors[],
-			 int      DecodedBits[],
-			 struct c_node c_nodes[],
-			 struct v_node v_nodes[],
-			 int	  CodeLength,
-			 int	  NumberParityBits,
-			 int	  max_iter,
-			 float    r_scale_factor,
-			 float    q_scale_factor, 
-			 int      data[]);
 
 #endif
