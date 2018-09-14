69eaa842ef72f21e422b6da4a6432c5f2e0c2b64
drowe67 <drowe67@01035d8c-6547-0410-b346-abe4f91aad63>
2018-09-14 23:27:54 +0000

    used 'tee' to give some feedback while test is running

--- codec2-dev/script/fskrx.sh
+++ codec2-dev/script/fskrx.sh
@@ -1,10 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 # fskrx.sh
 # David Rowe Sep 2018
 #
 # Receive 100 baud 2FSK test frames using RTL-SDR V3 dongle and nanosdr
 #
-# usage: $ sudo /fskrx.sh FreqHz logFileName.json
+# usage: $ sudo ./fskrx.sh FreqHz logFileName.json
 #        $ sudo ./fskrx.sh 7177000 20180912-1800.json
 #
 # To stop:
@@ -14,4 +14,4 @@
 CODEC2_BIN=/home/david/codec2-dev/build_linux/src
 NANORX_BIN=/home/david/nanosdr-0.75/build/nanorx
 
-sudo $NANORX_BIN/nanorx -i rtlsdr -f $1 -m USB --output-rate=48k | $CODEC2_BIN/fsk_demod -f -t 2 48000 100 - /dev/null 2>$2 
+sudo $NANORX_BIN/nanorx -i rtlsdr -f $1 -m USB --output-rate=48k | $CODEC2_BIN/fsk_demod -f -t 2 48000 100 - /dev/null 2> >(tee -a $2)
