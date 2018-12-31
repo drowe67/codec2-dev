bed4f6ad4aa73b43efc388fc7b7a6d464ae1d3f8
don_reid <don_reid@01035d8c-6547-0410-b346-abe4f91aad63>
2018-12-31 20:27:31 +0000

    Separate HRA_112_112.h into a header (with macros and external declarations), and a source file with the data. Take the oportunity to declare the data as "const" to help make more efficent code. Some casts are then needed to assign these to non "const" pointers in the utility.

--- codec2-dev/src/ldpc_dec.c
+++ codec2-dev/src/ldpc_dec.c
@@ -130,10 +130,10 @@ int main(int argc, char *argv[])
             ldpc.NumberRowsHcols = HRA_112_112_NUMBERROWSHCOLS;
             ldpc.max_row_weight = HRA_112_112_MAX_ROW_WEIGHT;
             ldpc.max_col_weight = HRA_112_112_MAX_COL_WEIGHT;
-            ldpc.H_rows = HRA_112_112_H_rows;
-            ldpc.H_cols = HRA_112_112_H_cols;
-            ainput = HRA_112_112_input;
-            adetected_data = HRA_112_112_detected_data;
+            ldpc.H_rows = (uint16_t *)HRA_112_112_H_rows;
+            ldpc.H_cols = (uint16_t *)HRA_112_112_H_cols;
+            ainput = (float *)HRA_112_112_input;
+            adetected_data = (char *)HRA_112_112_detected_data;
         }
     }
 
