fe70e3d90077e85880cbe622cb0911078547c4ad
don_reid <don_reid@01035d8c-6547-0410-b346-abe4f91aad63>
2019-01-08 21:58:29 +0000

    Optional memory allocation reporting.

---
+++ src/debug_alloc.h
@@ -0,0 +1,51 @@
+/* debug_alloc.h
+ *
+ * Some macros which can report on malloc results.
+ *
+ * Enable with "-D DEBUG_MALLOC"
+ */
+
+#ifndef DEBUG_ALLOC_H
+#define DEBUG_ALLOC_H
+
+#include <stdio.h>
+
+#ifdef DEBUG_ALLOC
+// Debug calls
+
+  static inline void * DEBUG_MALLOC(const char *func, size_t size) {
+    void *ptr = malloc(size);
+    fprintf(stderr, "MALLOC: %s %p %zd", func, ptr, size);
+    if (!ptr) fprintf(stderr, " ** FAILED **");
+    fprintf(stderr, "\n");
+    return(ptr);
+    }
+  #define MALLOC(size) DEBUG_MALLOC(__func__, size)
+
+  static inline void * DEBUG_CALLOC(const char *func, size_t nmemb, size_t size) {
+    void *ptr = calloc(nmemb, size);
+    fprintf(stderr, "CALLOC: %s %p %zd %zd", func, ptr, nmemb, size);
+    if (!ptr) fprintf(stderr, " ** FAILED **");
+    fprintf(stderr, "\n");
+    return(ptr);
+    }
+  #define CALLOC(nmemb, size) DEBUG_CALLOC(__func__, nmemb, size)
+
+  static inline void DEBUG_FREE(const char *func, void *ptr) {
+    free(ptr);
+    fprintf(stderr, "FREE: %s %p\n", func, ptr);
+    }
+  #define FREE(ptr) DEBUG_FREE(__func__, ptr)
+
+
+#else //DEBUG_ALLOC
+// Default to normal calls
+  #define MALLOC(size) malloc(size)
+
+  #define CALLOC(nmemb, size) calloc(nmemb, size)
+
+  #define FREE(ptr) free(ptr)
+
+#endif //DEBUG_ALLOC
+
+#endif //DEBUG_ALLOC_H
