9890cac32d49c806f819d2a1485b6f0ef6259afb
drowe67 <drowe67@01035d8c-6547-0410-b346-abe4f91aad63>
2019-01-11 20:40:52 +0000

    progressing utils for VQ training for LPCNet

--- misc/extract.c
+++ misc/extract.c
@@ -10,13 +10,23 @@ int main(int argc, char *argv[]) {
 
     fin = fopen(argv[1],"rb"); assert(fin != NULL);
     fout = fopen(argv[2],"wb"); assert(fout != NULL);
-    int st = atoi(argv[3]); int en = atoi(argv[4]);
-    printf("extracting from %d to %d...\n", st, en);
+    int st = atoi(argv[3]); int en = atoi(argv[4]); float scale = atof(argv[5]);
+    float pred = atof(argv[6]);
+    printf("extracting from %d to %d inclusive... scale factor = %f pred = %f\n", st, en, scale, pred);
+   
+    float features[NB_FEATURES], features_prev[NB_FEATURES], delta[NB_FEATURES];
+    int i;
     
-    float features[NB_FEATURES];
+    for(i=0; i<NB_FEATURES; i++)
+        features_prev[i] = 0.0;
 
-    while((fread(features, sizeof(float), NB_FEATURES, fin) == NB_FEATURES))
-        fwrite(&features[st], sizeof(float), en-st+1, fout);
+    while((fread(features, sizeof(float), NB_FEATURES, fin) == NB_FEATURES)) {
+        for(i=st; i<=en; i++) {
+            delta[i] = scale*(features[i] - pred*features_prev[i]);
+            features_prev[i] = features[i];
+        }
+        fwrite(&delta[st], sizeof(float), en-st+1, fout);
+    }
 
     fclose(fin); fclose(fout);
     
