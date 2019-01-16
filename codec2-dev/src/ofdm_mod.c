6bac80ece51e956aa6eb551e9d39ab71d55c979f
okcsampson <okcsampson@01035d8c-6547-0410-b346-abe4f91aad63>
2019-01-16 08:35:34 +0000

    Update ofdm mod/demod/gen to use getopt

--- codec2-dev/src/ofdm_mod.c
+++ codec2-dev/src/ofdm_mod.c
@@ -27,12 +27,15 @@
   along with this program; if not, see <http://www.gnu.org/licenses/>.
 */
 
+#define OPTPARSE_IMPLEMENTATION
+#define OPTPARSE_API static
+#include "optparse.h"
+
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
-#include <errno.h>
 
 #include "codec2_ofdm.h"
 #include "ofdm_internal.h"
@@ -40,20 +43,33 @@
 #include "gp_interleaver.h"
 #include "varicode.h"
 
+#define IS_DIR_SEPARATOR(c)     ((c) == '/')
+
 static struct OFDM_CONFIG *ofdm_config;
 
 static int ofdm_bitsperframe;
 static int ofdm_nuwbits;
 static int ofdm_ntxtbits;
 
-int opt_exists(char *argv[], int argc, char opt[]) {
-    int i;
-    for (i=0; i<argc; i++) {
-        if (strcmp(argv[i], opt) == 0) {
-            return i;
-        }
-    }
-    return 0;
+static const char *progname;
+
+void opt_help() {
+    fprintf(stderr, "\nusage: %s [options]\n\n", progname);
+    fprintf(stderr, "  --in      filename  Name of InputOneCharPerBitFile\n");
+    fprintf(stderr, "  --out     filename  Name of OutputModemRawFile\n");
+    fprintf(stderr, "  --nc      [17..64]  Number of Carriers (17 default, 64 max)\n");
+    fprintf(stderr, "  --tcp        Nsecs  Cyclic Prefix Duration (.002 default)\n");
+    fprintf(stderr, "  --ts         Nsecs  Symbol Duration (.018 default)\n");
+    fprintf(stderr, "  --testframes Nsecs  Transmit test frames (adjusts test frames for raw and LDPC modes)\n");
+    fprintf(stderr, "  --interleave depth  Interleave depth for LDPC frames, e.g. 1,2,4,8,16 (default is 1)\n");
+    fprintf(stderr, "  --tx_freq    freq   Set an optional modulation TX centre frequency (1500.0 default)\n");
+    fprintf(stderr, "  --rx_freq    freq   Set an optional modulation RX centre frequency (1500.0 default)\n\n");
+    fprintf(stderr, "  --verbose           Output variable assigned values to stderr (default off)\n");
+    fprintf(stderr, "  --txbpf             Transmit band pass filter boolean (default off)\n");
+    fprintf(stderr, "  --text              Include a standard text message boolean (default off)\n");
+    fprintf(stderr, "  --ldpc              Run LDPC decoder boolean. This forces 112, one char/bit output values\n"
+                    "                      per frame.  In testframe mode raw and coded errors will be counted\n");
+    exit(-1);
 }
 
 int main(int argc, char *argv[])
@@ -61,90 +77,172 @@ int main(int argc, char *argv[])
     FILE          *fin, *fout;
     struct OFDM   *ofdm;
     struct LDPC   ldpc;
-    int           frame;
-    int           i, j, arg;
-    int           testframes, ldpc_en, interleave_frames, use_text;
-   
-    if (argc < 3) {
-        fprintf(stderr, "\n");
-	fprintf(stderr, "usage: %s InputOneCharPerBitFile OutputModemRawFile [--ldpc] [--interleaver depth]\n\n", argv[0]);
-        fprintf(stderr, "  --nc      [17..64]  Number of Carriers (17 default, 64 max)\n");
-        fprintf(stderr, "  --tcp        Nsecs  Cyclic Prefix Duration (.002 default)\n");
-        fprintf(stderr, "  --ts         Nsecs  Symbol Duration (.018 default)\n");
-        fprintf(stderr, "  --testframes Nsecs  Transmit test frames (adjusts test frames for raw and LDPC modes)\n");
-        fprintf(stderr, "  --ldpc              Run LDPC decoder.  This forces 112, one char/bit output values\n"
-                        "                      per frame.  In testframe mode (--testframes) raw and coded errors\n"
-                        "                      will be counted\n");
-        fprintf(stderr, "  --interleave depth  Interleave depth for LDPC frames, e.g. 1,2,4,8,16, default is 1\n");
-        fprintf(stderr, "  --txbpf             Transmit band pass filter\n");
-        fprintf(stderr, "  --text              Include a standard text message\n");
-        fprintf(stderr, "  --tx_freq           Set an optional modulation TX centre frequency\n");
-        fprintf(stderr, "  --rx_freq           Set an optional modulation RX centre frequency\n");
-        fprintf(stderr, "\n");
-	exit(1);
+    float         tcp, ts, tx_centre, rx_centre;
+    char          *fin_name, *fout_name;
+    int           opt, frame, verbose;
+    int           i, j, val, nc;
+    int           testframes, Nframes, Nsec, Nrows, ldpc_en, interleave_frames, use_text;
+    int           txbpf_en, input_specified, output_specified;
+
+    char *pn = argv[0] + strlen (argv[0]);
+
+    while (pn != argv[0] && !IS_DIR_SEPARATOR (pn[-1]))
+        --pn;
+    
+    progname = pn;
+
+    /* See if they want help */
+
+    if (argc == 1) {
+        opt_help();
     }
 
-    if (strcmp(argv[1], "-")  == 0)
-        fin = stdin;
-    else if ( (fin = fopen(argv[1],"rb")) == NULL ) {
-        fprintf(stderr, "Error opening input file: %s: %s.\n", argv[1], strerror(errno));
-        exit(1);
+    /* Turn off stream buffering */
+
+    setvbuf(stdin, NULL, _IONBF, BUFSIZ);
+    setvbuf(stdout, NULL, _IONBF, BUFSIZ);
+
+    fin = stdin;
+    fout = stdout;
+
+    /* set for LDPC coded or uncoded frames */
+    
+    ldpc_en = 0;
+    interleave_frames = 1;
+
+    input_specified = 0;
+    output_specified = 0;
+    verbose = 0;
+    txbpf_en = 0;
+    testframes = 0;
+    use_text = 0;
+
+    Nframes = 0;
+    Nsec = 0;
+    Nrows = 0;
+
+    nc = 17;
+    tcp = 0.0020f;
+    ts =  0.0180f;
+    rx_centre = 1500.0f;
+    tx_centre = 1500.0f;
+
+    struct optparse options;
+
+    struct optparse_long longopts[] = {
+        {"in",         'a', OPTPARSE_REQUIRED},
+        {"out",        'b', OPTPARSE_REQUIRED},
+        {"nc",         'c', OPTPARSE_REQUIRED},
+        {"tcp",        'd', OPTPARSE_REQUIRED},
+        {"ts",         'e', OPTPARSE_REQUIRED},
+        {"testframes", 'f', OPTPARSE_REQUIRED},
+        {"interleave", 'g', OPTPARSE_REQUIRED},
+        {"tx_freq",    'h', OPTPARSE_REQUIRED},
+        {"rx_freq",    'i', OPTPARSE_REQUIRED},
+        {"ldpc",       'j', OPTPARSE_NONE},
+        {"txbpf",      'k', OPTPARSE_NONE},
+        {"text",       'l', OPTPARSE_NONE},
+        {"verbose",    'v', OPTPARSE_NONE},
+        {0, 0, 0}
+    };
+
+    optparse_init(&options, argv);
+
+    while ((opt = optparse_long(&options, longopts, NULL)) != -1) {
+        switch (opt) {
+            case '?':
+                opt_help();
+            case 'a':
+                fin_name = options.optarg;
+                input_specified = 1;
+                break;
+            case 'b':
+                fout_name = options.optarg;
+                output_specified = 1;
+                break;
+            case 'c':
+                val = atoi(options.optarg);
+                
+                if (val > 64 || val < 17) {
+                    opt_help();
+                } else {
+                    nc = val;
+                }
+                break;
+            case 'd':
+                tcp = atof(options.optarg);
+                break;
+            case 'e':
+                ts = atof(options.optarg);
+                break;
+            case 'f':
+                testframes = 1;
+                Nsec = atoi(options.optarg);
+                break;
+            case 'g':
+                interleave_frames = atoi(options.optarg);
+                break;
+            case 'h':
+                tx_centre = atof(options.optarg);
+                break;
+            case 'i':
+                rx_centre = atof(options.optarg);
+                break;
+            case 'j':
+                ldpc_en = 1;
+                break;
+            case 'k':
+                txbpf_en = 1;
+                break;
+            case 'l':
+                use_text = 1;
+                break;
+            case 'v':
+                verbose = 1;
+        }
     }
 
-    if (strcmp(argv[2], "-") == 0)
-        fout = stdout;
-    else if ( (fout = fopen(argv[2],"wb")) == NULL ) {
-        fprintf(stderr, "Error opening output modem sample file: %s: %s.\n", argv[2], strerror(errno));
-        exit(1);
+    /* Print remaining arguments to give user a hint */
+
+    char *arg;
+
+    while ((arg = optparse_arg(&options)))
+        fprintf(stderr, "%s\n", arg);
+
+    if (input_specified) {
+        if ((fin = fopen(fin_name, "rb")) == NULL) {
+            fprintf(stderr, "Error opening input bits file: %s\n", fin_name);
+            exit(-1);
+        }
+    }
+
+    if (output_specified) {
+        if ((fout = fopen(fout_name, "wb")) == NULL) {
+            fprintf(stderr, "Error opening output modem sample file: %s\n", fout_name);
+            exit(-1);
+        }
     }
     
     if ((ofdm_config = (struct OFDM_CONFIG *) calloc(1, sizeof (struct OFDM_CONFIG))) == NULL) {
         fprintf(stderr, "Out of Memory\n");
-        exit(1);
+        exit(-1);
     }
 
-    ofdm_config->fs = 8000.0;			/* Sample Frequency */
-    ofdm_config->ofdm_timing_mx_thresh = 0.30;
+    ofdm_config->fs = 8000.0f;			/* Sample Frequency */
+    ofdm_config->ofdm_timing_mx_thresh = 0.30f;
     ofdm_config->ftwindowwidth = 11;
     ofdm_config->state_str = 16; 		/* state string length */
     ofdm_config->bps = 2;   			/* Bits per Symbol */
     ofdm_config->txtbits = 4; 			/* number of auxiliary data bits */
     ofdm_config->ns = 8;  			/* Number of Symbol frames */
 
-    if ((arg = opt_exists(argv, argc, "--tx_freq"))) {
-        ofdm_config->tx_centre = atof(argv[arg+1]);	/* TX Centre Frequency */
-    } else {
-        ofdm_config->tx_centre = 1500.0;
-    }
+    ofdm_config->tx_centre = tx_centre;
+    ofdm_config->rx_centre = rx_centre;
+    ofdm_config->nc = nc;
+    ofdm_config->tcp = tcp;
+    ofdm_config->ts = ts;
 
-    if ((arg = opt_exists(argv, argc, "--rx_freq"))) {
-        ofdm_config->rx_centre = atof(argv[arg+1]);	/* RX Centre Frequency */
-    } else {
-        ofdm_config->rx_centre = 1500.0;
-    }
-
-    if ((arg = opt_exists(argv, argc, "--nc"))) {
-        ofdm_config->nc = atoi(argv[arg+1]);	/* Number of carriers */
-
-        if (ofdm_config->nc > 64 || ofdm_config->nc < 17)
-            ofdm_config->nc = 17;
-    } else {
-        ofdm_config->nc = 17;
-    }
-
-    if ((arg = opt_exists(argv, argc, "--tcp"))) {
-        ofdm_config->tcp = atof(argv[arg+1]);	/* Cyclic Prefix duration */
-    } else {
-        ofdm_config->tcp = 0.0020;
-    }
-
-    if ((arg = opt_exists(argv, argc, "--ts"))) {
-        ofdm_config->ts = atof(argv[arg+1]);	/* Symbol duration */
-    } else {
-        ofdm_config->ts = 0.0180;
-    }
-
-    ofdm_config->rs = (1.0f / ofdm_config->ts); /* Symbol Rate */
+    ofdm_config->rs = (1.0f / ts); /* Modulating Symbol Rate */
 
     ofdm = ofdm_create(ofdm_config);
     assert(ofdm != NULL);
@@ -166,20 +264,8 @@ int main(int argc, char *argv[])
     int coded_bits_per_frame = ldpc.coded_bits_per_frame;
     int Nbitsperframe;
 
-    /* set for LDPC coded or uncoded frames */
-    
-    ldpc_en = 0; interleave_frames = 1;
-
-    if (opt_exists(argv, argc, "--ldpc")) {
-
+    if (ldpc_en) {
         assert((ofdm_nuwbits+ofdm_ntxtbits+coded_bits_per_frame) == ofdm_bitsperframe); /* sanity check */
-
-        ldpc_en = 1;
-
-        if ((arg = opt_exists(argv, argc, "--interleave"))) {
-            interleave_frames = atoi(argv[arg+1]);
-        }
-
         Nbitsperframe = interleave_frames*data_bits_per_frame;
     } else {
         /* vanilla uncoded input bits mode */
@@ -188,10 +274,12 @@ int main(int argc, char *argv[])
     
     int Nsamperframe = ofdm_get_samples_per_frame();
 
-    fprintf(stderr, "data_bits_per_frame = %d\n", data_bits_per_frame);
-    fprintf(stderr, "coded_bits_per_frame  = %d\n", coded_bits_per_frame );
-    fprintf(stderr, "Nsamperframe: %d, interleave_frames: %d, Nbitsperframe: %d \n", 
-        Nsamperframe, interleave_frames, Nbitsperframe);
+    if (verbose) {
+        fprintf(stderr, "data_bits_per_frame = %d\n", data_bits_per_frame);
+        fprintf(stderr, "coded_bits_per_frame  = %d\n", coded_bits_per_frame );
+        fprintf(stderr, "Nsamperframe: %d, interleave_frames: %d, Nbitsperframe: %d \n", 
+            Nsamperframe, interleave_frames, Nbitsperframe);
+    }
 
     uint8_t tx_bits_char[Nbitsperframe];
     short   tx_scaled[Nsamperframe];
@@ -201,34 +289,24 @@ int main(int argc, char *argv[])
         txt_bits_char[i] = 0;
     }
    
-    testframes = 0;
-    use_text = 0;
-    int Nframes = 0;
-
-    if ((arg = (opt_exists(argv, argc, "--testframes")))) {
-        testframes = 1;
-        int Nsec, Nrows;
-        Nsec = atoi(argv[arg+1]);
+    if (testframes) {
         Nrows = Nsec*ofdm_config->rs;
         Nframes = floor((Nrows-1)/ofdm_config->ns);
 
-        fprintf(stderr, "Nframes: %d\n", Nframes);
+        if (verbose)
+            fprintf(stderr, "Nframes: %d\n", Nframes);
     }
 
-    if (opt_exists(argv, argc, "--txbpf")) {
+    if (txbpf_en) {
         ofdm_set_tx_bpf(ofdm, 1);
     }
 
-    if (opt_exists(argv, argc, "--text")) {
-        use_text = 1;
-    }
+    char    text_str[] = "cq cq cq hello world\r";    // Add text bits to match other tests
+    char    *ptr_text = text_str;
 
-// Add text bits to match other tests
-char text_str[] = "cq cq cq hello world\r";
-char *ptr_text = &text_str[0];
-short                tx_varicode_bits[VARICODE_MAX_BITS];
-int                  nvaricode_bits = 0;
-int                  varicode_bit_index = 0;
+    short   tx_varicode_bits[VARICODE_MAX_BITS];
+    int     nvaricode_bits = 0;
+    int     varicode_bit_index = 0;
 
     /* main loop ----------------------------------------------------------------*/
     
@@ -258,7 +336,10 @@ int                  varicode_bit_index = 0;
                             /* get new char and encode */
                             char s[2];
                             s[0] = *ptr_text++;
-                            if (*ptr_text == 0) ptr_text = &text_str[0];
+                            
+                            if (*ptr_text == 0)
+                                ptr_text = &text_str[0];
+                            
                             nvaricode_bits = varicode_encode(tx_varicode_bits, s, VARICODE_MAX_BITS, 1, 1);
                             varicode_bit_index = 0;
                         }
@@ -330,25 +411,20 @@ int                  varicode_bit_index = 0;
             fwrite(tx_scaled, sizeof(short), Nsamperframe, fout);
         }
         
-	/* if this is in a pipeline, we probably don't want the usual
-	   buffering to occur */
-
-        if (fout == stdout) fflush(stdout);
-        if (fin == stdin) fflush(stdin);
-
         frame++;
 
-        if (testframes && (frame >= Nframes)) {
-            goto finished;
-        }
-
+        if (testframes && (frame >= Nframes))
+            break;
     }
 
- finished:
-    fclose(fin);
-    fclose(fout);
+    if (input_specified)
+        fclose(fin);
 
-    fprintf(stderr, "%d frames processed\n", frame);
+    if (output_specified)
+        fclose(fout);
+
+    if (verbose)
+        fprintf(stderr, "%d frames processed\n", frame);
 
     ofdm_destroy(ofdm);
 
