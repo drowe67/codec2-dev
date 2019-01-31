c6bc4e63f865e618996ce249e42f11e0f0a9def3
drowe67 <drowe67@01035d8c-6547-0410-b346-abe4f91aad63>
2019-01-31 19:51:30 +0000

    Brads latest and greatest, tweaked function name to match filename

--- codec2-dev/octave/ldpc_gen_c_h_file.m
+++ codec2-dev/octave/ldpc_gen_c_h_file.m
@@ -9,61 +9,80 @@
 %
 % Inputs:
 % First parameter  - name of file with LDPC codes
-% Second parameter - max_iterations, defaults to 100
+% Second parameter - , defaults to 100
 % Third parameter  - decoder_type, defaults to 0
-% Fourth parameter - input array, optional
-% Fifth parameter  - detected data array, optional
 %
 % Output: Two files with the same filename as the LDPC input, but with .c and .h
 % extensions.
-
+ 
 function ldpc_gen_c_h_file(varargin)
- 
+    
     ldpc  % load ldpc functions
- 
-    if nargin == 0
-        printf("Error - you must specify a file containing the LDPC codes (e.g. HRA_112_112.txt).\n");
-        return;
-    end
- 
-    loadStr = varargin{1};
-   
-    if nargin == 1
-        max_iterations = 100;
-        decoder_type = 0;
-    else
-        max_iterations = varargin{2};
-        decoder_type = varargin{3};
-    end
-  
-    mod_order = 4; bps = 2; modulation = 'QPSK'; mapping = 'gray';
+    ldpc_fsk_lib  % for ldpc_encode
+    
     % Assuming cml has been installed in the users' home folder, which is the
     % default install location
     init_cml('~/cml/');
     
-    % When calling 'load' this way, it returns a struct.  The code assumes the
-    % struct has one element, and the one/first element is the array
-    % to process
-    tempStruct = load(loadStr);
-    b = fieldnames(tempStruct);
-    ldpcArrayName = b{1,1};
+    if nargin == 0
+        printf("Error - you must specify a file containing the LDPC codes (e.g. HRA_112_112.txt).\n");
+        return;
+    end
+    loadStr = varargin{1};
+  
  
-    % extract the array from the struct
-    ldpcArray = tempStruct.(ldpcArrayName);
-   
     % The ldpc variable name may not be what we want for a file/variable names, but
     % the load filename will be, so use it.
-    [~,ldpcArrayName,~] = fileparts(loadStr);
+    [~,ldpcArrayName,ext] = fileparts(loadStr);
     includeFileName = strcat(ldpcArrayName, '.h');
     sourceFileName = strcat(ldpcArrayName,  '.c');
+   
+    % Get the ext of the file first.  If it's a txt, then do what we
+    % are doing.  If .mat, then just load, knowing the variable is HRA
+    if strcmp(ext, '.mat') == 1
+        load(loadStr);
+    else
+        % When calling 'load' this way, it returns a struct.  The code assumes the
+        % struct has one element, and the one/first element is the array
+        % to process
+        tempStruct = load(loadStr);
+        b = fieldnames(tempStruct);
+        ldpcArrayName = b{1,1};
+        % extract the array from the struct
+        HRA = tempStruct.(ldpcArrayName);
+    endif
+
     
-    [code_param framesize rate] = ldpc_init_user(ldpcArray, modulation, mod_order, mapping);
+    max_iterations = 100;
+    decoder_type = 0;
+ 
+    % user overloads 
+    if nargin == 2
+        max_iterations = varargin{2};
+    end
+ 
+    if nargin == 3
+        decoder_type = varargin{3};
+    end
+ 
+   
+    % the tests are performed using BPSK modulation, but in practice codes can be used
+    % with other modulation, e.g. QPSK
+    mod_order = 2; modulation = 'BPSK'; mapping = 'gray';
+   
+    [code_param framesize rate] = ldpc_init_user(HRA, modulation, mod_order, mapping);
+  
+    code_length = code_param.symbols_per_frame;
+    code_length % reported as 112.
+  
+    % *********************  test for enc/dec
+   [input_decoder_c, detected_data] = genInputOutputData(code_param, max_iterations, decoder_type);
+     
    
     % First, create the H file
     f = fopen(includeFileName, "wt");
- 
     printHeader(f, includeFileName, ldpcArrayName, mfilename());
-  
+ 
     fprintf(f,"#define %s_NUMBERPARITYBITS %d\n", ldpcArrayName, rows(code_param.H_rows));
     fprintf(f,"#define %s_MAX_ROW_WEIGHT %d\n", ldpcArrayName, columns(code_param.H_rows));
     fprintf(f,"#define %s_CODELENGTH %d\n", ldpcArrayName, code_param.symbols_per_frame);
@@ -74,25 +93,21 @@ function ldpc_gen_c_h_file(varargin)
     fprintf(f,"\n");
     fprintf(f,"extern const uint16_t %s_H_rows[];\n", ldpcArrayName);
     fprintf(f,"extern const uint16_t %s_H_cols[];\n", ldpcArrayName);
-   
-    if nargin == 5
-        fprintf(f,"extern const float %s_input[];\n", ldpcArrayName);
-        fprintf(f,"extern const char %s_detected_data[];\n\n", ldpcArrayName);
-    end
-   
+  
+    fprintf(f,"extern const float %s_input[];\n", ldpcArrayName);
+    fprintf(f,"extern const char %s_detected_data[];\n\n", ldpcArrayName);
+  
     fclose(f);
-   
+  
     
     % Then, the C file
     f = fopen(sourceFileName, "wt");
- 
     printHeader(f, sourceFileName, ldpcArrayName, mfilename());
- 
     fprintf(f, "#include <stdint.h>\n");
-    fprintf(f, "#include \"%s\"\n", includeFileName);   
+    fprintf(f, "#include \"%s\"\n", includeFileName);  
     
     % clock out 2D array to linear C array in row order ....
-    fprintf(f,"\ndouble %s_H_rows[] = {\n", ldpcArrayName);
+    fprintf(f,"\nconst uint16_t %s_H_rows[] = {\n", ldpcArrayName);
     [r c] = size(code_param.H_rows);
     for j=1:c
         for i=1:r
@@ -104,7 +119,8 @@ function ldpc_gen_c_h_file(varargin)
             end
         end
     end
-    fprintf(f,"\ndouble %s_H_cols[] = {\n", ldpcArrayName);
+ 
+    fprintf(f,"\nconst uint16_t %s_H_cols[] = {\n", ldpcArrayName);
     [r c] = size(code_param.H_cols);
     for j=1:c
         for i=1:r
@@ -116,36 +132,58 @@ function ldpc_gen_c_h_file(varargin)
             end
         end
     end
-    % If the input and detected output have been specified, then print them
-    if nargin == 5
-      input_decoder_c = varargin{4};
-      detected_data = varargin{5};
  
-        fprintf(f,"\ndouble input[] = {\n");
-        for i=1:length(input_decoder_c)
-            fprintf(f, "%.17g", input_decoder_c(i));
-            if i == length(input_decoder_c)
-                fprintf(f,"\n};\n");
-            else
-                fprintf(f,", ");          
-            end
+    % input and detected_data arrays
+    fprintf(f,"const float %s_input[] = {\n", ldpcArrayName);
+    for i=1:length(input_decoder_c)
+        fprintf(f, "%.17g", input_decoder_c(i));
+        if i == length(input_decoder_c)
+            fprintf(f,"\n};\n");
+        else
+            fprintf(f,", ");         
         end
+    end
+    fprintf(f,"const char %s_detected_data[] = {\n", ldpcArrayName);
+    for i=1:length(detected_data)
+        fprintf(f, "%d", detected_data(i));
+        if i == length(detected_data)
+            fprintf(f,"\n};\n");
+        else
+            fprintf(f,", ");         
+        end
+    end
  
-        fprintf(f,"\nchar detected_data[] = {\n");
-        for i=1:length(detected_data)
-            fprintf(f, "%d", detected_data(i));
-            if i == length(detected_data)
-                fprintf(f,"\n};\n");
-            else
-                fprintf(f,", ");          
-            end
-        end
-    end % if nargin == 5
     fclose(f);
 endfunction
- 
  
 function printHeader(f, includeFileName, ldpcArrayName, mFilename)
     fprintf(f, "/*\n  FILE....: %s\n\n", includeFileName);
     fprintf(f, "  Static arrays for LDPC codec %s, generated by %s.m.\n*/\n\n", ldpcArrayName, mFilename);
+endfunction
+ 
+function [input_decoder_c, detected_data] = genInputOutputData(code_param, max_iterations, decoder_type)
+  
+    % borrowed from test_ldpc_fsk_lib.m, simple_ut
+    EsNodB = 3;
+    data = round( rand( 1, code_param.data_bits_per_frame ) );
+    codeword = ldpc_encode(code_param, data);    %defined in ldps_fsk_lib.
+ 
+    s = 1 - 2 * codeword;  
+    %aa = code_param.symbols_per_frame  % test - this WAS 112, then it gets set to 224???
+    %code_param.symbols_per_frame = length( s )
+ 
+    EsNo = 10^(EsNodB/10);
+    variance = 1/(2*EsNo);
+    noise = sqrt(variance)* randn(1,code_param.symbols_per_frame);
+    r = s + noise;
+   
+    % borrowed from ldpc_fsk_lib.m, ldpc_decode
+    llr = sd_to_llr(r);
+ 
+    [x_hat, PCcnt] = MpDecode(llr, code_param.H_rows, code_param.H_cols, ...
+                            max_iterations, decoder_type, 1, 1);        
+    Niters = sum(PCcnt!=0);
+    detected_data = x_hat(Niters,:);
+ 
+    input_decoder_c = llr;
 endfunction
