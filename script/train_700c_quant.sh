757aa66ab4ea1fe569ac2ec894e65c2cb04b07b9
David <david@rowetel.com>
2019-05-04 12:06:59 +0930

    this version of script tests effect of post filter and bypassing VQ

--- script/train_700c_quant.sh
+++ script/train_700c_quant.sh
@@ -8,8 +8,9 @@
 SRC=~/Downloads/all_speech_8k.sw
 CODEC2_BUILD=/home/david/codec2/build_linux
 K=20
-SAMPLES=~/tmp/c2vec_mid
+SAMPLES=~/tmp/c2vec_pass
 
+# train a new VQ
 function train() {
   # c2enc can dump "feature vectors" that contain the current VQ input
   $CODEC2_BUILD/src/c2enc 700C $SRC /dev/null --mlfeat feat.f32
@@ -19,36 +20,64 @@ function train() {
   $CODEC2_BUILD/misc/vqtrain stage1_in.f32 $K 512 vq_stage2.f32 -s 1e-3
 }
 
+# encode/decode a file with the stock codec2 700C VQ
 function test_a() {
   b=$(basename "$1" .raw)
   $CODEC2_BUILD/src/c2enc 700C $1'.raw' - --var | $CODEC2_BUILD/src/c2dec 700C - - | sox -q -t .s16 -c 1 -r 8000 -b 16  - $SAMPLES/$b'_a.wav'
 }
 
+# stock 700C VQ like test_a, but no postfilter
+function test_a_nopf() {
+  b=$(basename "$1" .raw)
+  $CODEC2_BUILD/src/c2enc 700C $1'.raw' - --var | $CODEC2_BUILD/src/c2dec 700C - - --nopf | \
+  sox -q -t .s16 -c 1 -r 8000 -b 16  - $SAMPLES/$b'_a_nopf.wav'
+}
+
+# encode/decode a file with the new VQ we just trained above
 function test_b() {
   b=$(basename "$1" .raw)
   $CODEC2_BUILD/src/c2enc 700C $1'.raw' - --loadcb 1 vq_stage1.f32 --loadcb 2 vq_stage2.f32 --var | \
   $CODEC2_BUILD/src/c2dec 700C - - --loadcb 1 vq_stage1.f32 --loadcb 2 vq_stage2.f32 | sox -q -t .s16 -c 1 -r 8000 -b 16  - $SAMPLES/$b'_b.wav'
 }
 
+# just like b but no postfilter
+function test_b_nopf() {
+  b=$(basename "$1" .raw)
+  $CODEC2_BUILD/src/c2enc 700C $1'.raw' - --loadcb 1 vq_stage1.f32 --loadcb 2 vq_stage2.f32 --var | \
+  $CODEC2_BUILD/src/c2dec 700C - - --loadcb 1 vq_stage1.f32 --loadcb 2 vq_stage2.f32 $2 --nopf | \
+  sox -q -t .s16 -c 1 -r 8000 -b 16  - $SAMPLES/$b'_b_nopf.wav'
+}
+
+# pass an unquantised rate K vector through to the decoder as a control
+function test_uq() {
+  b=$(basename "$1" .raw)
+  $CODEC2_BUILD/src/c2enc 700C $1'.raw' $b'.bin' --mlfeat $b'_feat.f32'
+  $CODEC2_BUILD/misc/extract -s 1 -e 20 -t 41 $b'_feat.f32' $b'_ratek.f32'
+  $CODEC2_BUILD/src/c2dec 700C $b'.bin' - --loadratek $b'_ratek.f32' | sox -q -t .s16 -c 1 -r 8000 -b 16  - $SAMPLES/$b'_uq.wav'
+}
+
 function feat() {
   b=$(basename "$1" .raw)
   $CODEC2_BUILD/src/c2enc 700C $1'.raw' /dev/null --mlfeat $b.f32
 }
+
+# generate a bunch of test samples for a listening test
 function listen() {
-  # generate a bunch of test samples for comparsion 
-  mkdir -p $SAMPLES
   RAW_FILES="../raw/hts1a ../raw/hts2a ../raw/vk5qi ../raw/cq_ref ../raw/ve9qrp_10s $HOME/Downloads/ma01_01 $HOME/Downloads/c01_01_8k"
   for f in $RAW_FILES
   do
     test_a $f
+    test_a_nopf $f
     test_b $f
+    test_b_nopf $f
+    test_uq $f
   done
 }
 
-train
+mkdir -p $SAMPLES
+#train
 listen
-#feat ../raw/hts1a
-#feat $HOME/Downloads/ma01_01
+
 
 
 
