b44eec8ee84eae5d264aed094fbbd8ee6576c299
srsampson <coupaydeville@gmail.com>
2019-06-17 11:45:24 -0500

    Add temporary fix to tofdm.c

--- unittest/tofdm.c
+++ unittest/tofdm.c
@@ -208,6 +208,10 @@ int main(int argc, char *argv[])
     ofdm = ofdm_create(ofdm_config_default);
     assert(ofdm != NULL);
     ofdm_config = ofdm_get_config_param();
+
+    /////////////// needs option added above
+    ofdm_set_phase_est_bandwidth_mode(ofdm, HIGH_PHASE_EST);
+    ///////////////
     
     // make local copies for convenience
     ofdm_tx_centre = ofdm_config->tx_centre;
