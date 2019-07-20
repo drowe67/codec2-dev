440304293213926246ccc2783ddbc59998341c30
David <david@rowetel.com>
2019-07-21 06:12:16 +0930

    block adaptive working pretty well, except one sample (ve9qrp_10s) messed up by EQ

--- octave/vq_700c_eq.m
+++ octave/vq_700c_eq.m
@@ -46,7 +46,7 @@ function errors = vq_targets(vq, targets)
 endfunction
 
 
-% single stage vq a target matrix with adaptive EQ
+% single stage vq a target matrix with adaptive EQ, this didn't work
 
 function [errors eqs] = vq_targets_adap_eq(vq, targets, eqs)
   errors = []; gain=0.02;
@@ -59,6 +59,26 @@ function [errors eqs] = vq_targets_adap_eq(vq, targets, eqs)
     error = t - vq(index_list(1),:);
     eq = (1-gain)*eq + gain*error;
     errors = [errors; error]; eqs = [eqs; eq];
+  end
+endfunction
+
+
+% single stage vq a target matrix with block adaptive EQ, this works
+% well with nblock == 10
+
+function [errors eq] = vq_targets_block_eq(vq, targets, eq, nblock)
+  errors = []; n = 0; [tmp K] = size(vq); error_eq = zeros(1,K);
+  for i=1:length(targets)
+    t = targets(i,:) - eq;
+    [mse_list index_list] = search_vq(vq, t, 1);
+    error = t - vq(index_list(1),:);
+    error_eq += error;
+    errors = [errors; error];
+    n++;
+    if n == nblock
+      eq += error_eq/nblock;
+      n = 0; error_eq = zeros(1,K);
+    end  
   end
 endfunction
 
@@ -144,7 +164,7 @@ function table_across_samples
   vq2 = load(sprintf("%s_2.txt", vq_name));
   
   printf("----------------------------------------------------------------------------------\n");
-  printf("Sample                Initial  vq1     vq1_eq2  vq1_eq2  vq2  vq2_eq1  vq2_eq2 \n");
+  printf("Sample                Initial  vq1     vq1_eq2  vq1_eq2  vq2    vq2_eq1  vq2_eq2 \n");
   printf("----------------------------------------------------------------------------------\n");
             
   fn_targets = { "cq_freedv_8k_lfboost" "cq_freedv_8k_hfcut" "cq_freedv_8k" "hts1a" "hts2a" "cq_ref" "ve9qrp_10s" "vk5qi" "c01_01_8k" "ma01_01"};
@@ -160,12 +180,7 @@ function table_across_samples
     
     errors1 = vq_targets(vq1, targets);
     errors1_eq1 = vq_targets(vq1, targets-eq1);
-    eq2 = zeros(1,K);
-    for it=1:3
-      t = targets - eq2;
-      errors1_eq2 = vq_targets(vq1, t);    
-      eq2 += est_eq(vq1, t);
-    end
+    [errors1_eq2 eq2] = vq_targets_block_eq(vq1, targets, zeros(1,K), 10);
     
     % two stage mbest VQ --------------
     
@@ -182,14 +197,14 @@ function table_across_samples
       save_f32(sprintf("../script/%s_vq2_as.f32", fn_targets{i}), targets_);
       save_f32(sprintf("../script/%s_vq2_as_eq.f32", fn_targets{i}), targets_eq_);
     end 
-    printf("%-21s %6.2f  %6.2f  %6.2f   %6.2f     %6.2f  %6.2f  %6.2f\n", fn_targets{i},
+    printf("%-21s %6.2f  %6.2f  %6.2f   %6.2f   %6.2f  %6.2f  %6.2f\n", fn_targets{i},
             var(targets(:)), var(errors1(:)), var(errors1_eq1(:)), var(errors1_eq2(:)),
             var(errors2(:)), var(errors2_eq1(:)), var(errors2_eq2(:)));
 
     figure(figs++); clf;
     %plot(var(errors2'),'b;vq2;'); hold on; plot(var(errors2_eq1'),'g;vq2_eq1;'); plot(var(errors2_eq2'),'r;vq2_eq2;'); hold off;
-    plot(mean(targets); hold on; plot(mean(vq)); plot(eq2); hold off;
-    title(fn_targets{i});
+    plot(mean(targets),'g;mean(targets);'); hold on; plot(mean(vq1),'g;mean(vq1);'); plot(eq2,'r;eq2;'); hold off;
+    title(fn_targets{i}); axis([1 K -20 30]);
    end
 endfunction
 
@@ -252,7 +267,7 @@ endfunction
 
 % Experiment to test iterative approach of block update and remove
 % mean (ie frame energy), shows some promise at reducing HF energy
-% over several iterations while not affecting alreayd good samples
+% over several iterations while not affecting already good samples
 
 function experiment_iterate_block(fn_vq_txt, fn_target_f32)
   K = 20;
@@ -266,38 +281,12 @@ function experiment_iterate_block(fn_vq_txt, fn_target_f32)
   figure(2); clf; hold on;
   eq = zeros(1,K);
   for i=1:3
-    t = targets - eq;
-    errors = vq_targets(vq, t);    
-    eq += est_eq(vq, t);
-    figure(1); plot(mean(t));
+    [errors eq] = vq_targets_block_eq(vq, targets, eq, 10);    
+    figure(1); plot(mean(targets-eq));
     figure(2); plot(eq);
     printf("i: %d %6.2f\n", i, var(errors(:)))
   end
 endfunction
-
-% adaptive version of above
-
-function experiment_iterate_adap(fn_vq_txt, fn_target_f32)
-  K = 20;
-  vq = load("train_120_1.txt");
-  [targets e] = load_targets(fn_target_f32);
-
-  figure(3); clf;
-  plot(mean(targets),'b;mean(targets);');
-  hold on;
-  plot(mean(vq), 'g;mean(vq);');
-  figure(4); clf; hold on;
-  eqs = zeros(1,K);
-  for i=1:3
-    [errors eqs] = vq_targets_adap_eq(vq, targets, eqs);
-    t = targets - eqs(end,:);
-    figure(3); plot(mean(t));
-    figure(4); plot(eqs(end,:));
-    printf("i: %d %6.2f\n", i, var(errors(:)))
-  end
-  figure(5); clf; mesh(eqs);
-endfunction
-
 
 more off
 
@@ -305,8 +294,7 @@ more off
 % You'll need to run scripts/train_700C_quant.sh first to generate the .f32 files
 
 %interactive("train_120_1.txt", "cq_freedv_8k_lfboost.f32")
-table_across_samples;
+%table_across_samples;
 %vq_700c_plots({"hts1a.f32" "hts2a.f32" "ve9qrp_10s.f32" "ma01_01.f32" "train_120_1.txt"})
 %vq_700c_plots({"ve9qrp_10s.f32" "cq_freedv_8k_lfboost.f32" "cq_freedv_8k_hfcut.f32" "cq_freedv_8k.f32"})
-%experiment_iterate_block("train_120_1.txt", "cq_freedv_8k_lfboost.f32")
-%experiment_iterate_adap("train_120_1.txt", "cq_freedv_8k_lfboost.f32")
+experiment_iterate_block("train_120_1.txt", "ve9qrp_10s.f32")
