drowe67/codec2-dev

commit b8a8f669dc44778b5934e000b3456ab064853da5

modified ldpc_noise to generate correct real nosie for single sided No, ldpc_dec counts raw errors now, and refactored LLR mapping so it doesn't modify input

src/mpdecode_core.c
@@ -738,23 +738,17 @@ void sd_to_llr(double llr[], double sd[], int n) {
         sum += fabs(sd[i]);
     mean = sum/n;
                 
-    /* scale by mean to map onto +/- 1 symbol position */
-
-    for(i=0; i<n; i++) {
-        sd[i] /= mean;
-    }
-
     /* find variance from +/-1 symbol position */
 
     sum = sumsq = 0.0; 
     for(i=0; i<n; i++) {
         sign = (sd[i] > 0.0) - (sd[i] < 0.0);
-        x = (sd[i] - sign);
+        x = (sd[i]/mean - sign);
         sum += x;
         sumsq += x*x;
     }
-    mean = sum/n;
-    estvar = sumsq/n - mean*mean;
+    estvar = (n * sumsq - sum * sum) / (n * (n - 1));
+    //fprintf(stderr, "mean: %f var: %f\n", mean, estvar);
 
     estEsN0 = 1.0/(2.0 * estvar + 1E-3); 
     for(i=0; i<n; i++)

src/ldpc_dec.c
@@ -60,7 +60,7 @@ int main(int argc, char *argv[])
     char        *adetected_data;
     struct LDPC ldpc;
     double     *ainput;
-    int         Tbits, Terrs;
+    int         Tbits, Terrs, Tbits_raw, Terrs_raw;
     
     if (argc < 2) {
         fprintf(stderr, "\n");
@@ -140,7 +140,9 @@ int main(int argc, char *argv[])
     CodeLength = ldpc.CodeLength;                    /* length of entire codeword */
     NumberParityBits = ldpc.NumberParityBits;
     data_bits_per_frame = ldpc.NumberRowsHcols;
-    char ibits[data_bits_per_frame], out_char[CodeLength];
+    unsigned char ibits[data_bits_per_frame];
+    unsigned char pbits[NumberParityBits];
+    char out_char[CodeLength];
 
     testframes = 0;
     
@@ -215,7 +217,8 @@ int main(int argc, char *argv[])
             for(i=0; i<data_bits_per_frame; i++) {
                 ibits[i] = r[i] > 16384;
             }
-            Tbits = Terrs = 0;
+            encode(&ldpc, ibits, pbits);  
+            Tbits = Terrs = Tbits_raw = Terrs_raw = 0;
        }
 
         double *input_double = calloc(CodeLength, sizeof(double));
@@ -233,6 +236,24 @@ int main(int argc, char *argv[])
 
         while(fread(&input_double[offset], sizeof(double), nread, fin) == nread) {
             if (sdinput) {
+                if (testframes) {
+                    char in_char;
+                    for (i=0; i<data_bits_per_frame; i++) {
+                        in_char = input_double[i] < 0;
+                        if (in_char != ibits[i]) {
+                            Terrs_raw++;
+                        }
+                        Tbits_raw++;
+                    }
+                    for (i=0; i<NumberParityBits; i++) {
+                        in_char = input_double[i+data_bits_per_frame] < 0;
+                        if (in_char != pbits[i]) {
+                            Terrs_raw++;
+                        }
+                        Tbits_raw++;
+                    }
+                }
+
                 sd_to_llr(input_double, input_double, CodeLength);
             }
 
@@ -262,7 +283,7 @@ int main(int argc, char *argv[])
                     next_state = state;
                     switch(state) {
                     case 0:
-                        if (iter < MAX_ITER) {
+                        if (iter < ldpc.max_iter) {
                             /* OK we've found which frame to sync on */
                             next_state = 1;
                             frame = 0;
@@ -304,7 +325,9 @@ int main(int argc, char *argv[])
     }
 
     if (testframes) {
-        fprintf(stderr, "Tbits: %d Terr: %d BER: %4.3f\n", Tbits, Terrs, (float)Terrs/(Tbits+1E-12));
+        fprintf(stderr, "Raw Tbits..: %d Terr: %d BER: %4.3f\n", Tbits_raw, Terrs_raw,
+                (float)Terrs_raw/(Tbits_raw+1E-12));
+        fprintf(stderr, "Coded Tbits: %d Terr: %d BER: %4.3f\n", Tbits, Terrs, (float)Terrs/(Tbits+1E-12));
     }
     
     return 0;

src/ldpc_noise.c
@@ -57,7 +57,7 @@ int main(int argc, char *argv[]) {
     double No = pow(10.0, NodB/10.0);
     double sum_xx = 0; double sum_x = 0.0; long n = 0;
     
-    fprintf(stderr, "NodB = %f, No = %f\n", NodB, No);
+    fprintf(stderr, "single sided NodB = %f, No = %f\n", NodB, No);
     
     while (fread(&datain, sizeof(double), 1, fin) == 1) {
 
@@ -66,12 +66,12 @@ int main(int argc, char *argv[]) {
         double y = (double)random() / RAND_MAX;
         double z = sqrt(-2 * log(x)) * cos(2 * M_PI * y);
 
-	double noise = sqrt(No) * z;
+	double noise = sqrt(No/2) * z;
 	dataout = datain + noise;
 
         fwrite(&dataout, sizeof(double), 1, fout);        
 
-        // keep running stats to calulateactual noise variance (power)
+        // keep running stats to calculate actual noise variance (power)
         
         sum_xx += noise*noise;
         sum_x  += noise;
@@ -82,7 +82,7 @@ int main(int argc, char *argv[]) {
     fclose(fout); 
 
     double noise_var = (n * sum_xx - sum_x * sum_x) / (n * (n - 1));
-    fprintf(stderr, "measured noise power: %f\n", noise_var);
+    fprintf(stderr, "measured double sided (real) noise power: %f\n", noise_var);
  
     return 0;
 }

commit 8507c2e3ea2ce4d52ed5578b11d346cef79b4eb2

Fix log likelyhood.
Fixes: b8a8f669dc "modified ldpc_noise ... and refactored LLR mapping"
	which removed a normalisation step.

src/mpdecode_core.c
@@ -591,7 +591,7 @@ void sd_to_llr(float llr[], double sd[], int n) {
 
     estEsN0 = 1.0/(2.0L * estvar + 1E-3);
     for(i=0; i<n; i++)
-        llr[i] = 4.0L * estEsN0 * sd[i];
+        llr[i] = 4.0L * estEsN0 * sd[i] / mean;
 }
 
 