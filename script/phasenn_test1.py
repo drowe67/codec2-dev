a62f8c3f60405996a238c42e8b9ca2b66523a2a8
David <david@rowetel.com>
2019-08-18 13:53:22 +0930

    loss 0.0265 w between 0 and pi/4

--- script/phasenn_test1.py
+++ script/phasenn_test1.py
@@ -11,15 +11,15 @@ import sys
 from keras.layers import Dense
 from keras import models,layers
 from keras import initializers
+import matplotlib.pyplot as plt
 
 # constants
 
 N                 = 80      # number of samples in frames
-nb_samples        = 100000 
-nb_epochs         = 80
+nb_samples        = 100000
+nb_epochs         = 200
 np_input          = 3
 nb_output         = 2
-levels            = 16
 
 # Generate training data.  Given the phase at the start of the frame,
 # and the frequency, determine the phase at the end of the frame
@@ -32,23 +32,25 @@ phase_start = np.zeros(nb_samples)
 phase_end = np.zeros(nb_samples)
 for i in range(nb_samples):
     x=np.random.rand(2)
-    w[i] = x[0]*np.pi
-    w[i] = quant(w[i], levels)
+    #w[i] = np.pi/4 + 0.1*x[0]*np.pi
+    w[i] = x[0]*np.pi/4
     phase_start[i] = -np.pi + x[1]*2*np.pi
-    phase_start[i] = quant(phase_start[i], levels)
-    phase_end[i] = phase_start[i] + N*w[i]
-    phase_end[i] = quant(phase_end[i], levels)
+    phase_end[i] = phase_start[i] + N*w[i];
                      
 train  = np.column_stack((w, np.cos(phase_start), np.sin(phase_start)))
 target = np.column_stack((np.cos(phase_end), np.sin(phase_end)))
 print(train.shape)                       
 print(target.shape)
+
+#plt.figure(1)
+#plt.hist(w)
+#plt.show()
                         
 model = models.Sequential()
 model.add(layers.Dense(256, activation='relu', input_dim=np_input))
-model.add(layers.Dense(256, activation='relu'))
-model.add(layers.Dense(256, activation='relu'))
+model.add(layers.Dense(256, activation='relu', input_dim=np_input))
 model.add(layers.Dense(nb_output, activation='linear'))
+model.summary()
 
 # Compile our model 
 
@@ -59,15 +61,12 @@ model.compile(loss='mse', optimizer='adam')
 
 history = model.fit(train, target, validation_split=0.2, batch_size=32, epochs=nb_epochs)
 
-import matplotlib.pyplot as plt
-
 plot_en = 1;
 if plot_en:
     plt.figure(1)
-    plt.plot(np.sqrt(history.history['loss']))
-    plt.plot(np.sqrt(history.history['val_loss']))
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
     plt.title('model loss')
-    plt.ylabel('rms error')
     plt.xlabel('epoch')
     plt.legend(['train', 'valid'], loc='upper right')
     plt.show()
