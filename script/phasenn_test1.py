e9aa03e95dc343069d884bb3a804c2ae43345fef
David <david@rowetel.com>
2019-08-18 14:46:23 +0930

    loss < 0.01 w between pi/8 and pi/4

--- script/phasenn_test1.py
+++ script/phasenn_test1.py
@@ -17,7 +17,7 @@ import matplotlib.pyplot as plt
 
 N                 = 80      # number of samples in frames
 nb_samples        = 100000
-nb_epochs         = 200
+nb_epochs         = 20
 np_input          = 3
 nb_output         = 2
 
@@ -33,7 +33,7 @@ phase_end = np.zeros(nb_samples)
 for i in range(nb_samples):
     x=np.random.rand(2)
     #w[i] = np.pi/4 + 0.1*x[0]*np.pi
-    w[i] = x[0]*np.pi/4
+    w[i] = np.pi/8 + x[0]*np.pi/8
     phase_start[i] = -np.pi + x[1]*2*np.pi
     phase_end[i] = phase_start[i] + N*w[i];
                      
@@ -41,10 +41,6 @@ train  = np.column_stack((w, np.cos(phase_start), np.sin(phase_start)))
 target = np.column_stack((np.cos(phase_end), np.sin(phase_end)))
 print(train.shape)                       
 print(target.shape)
-
-#plt.figure(1)
-#plt.hist(w)
-#plt.show()
                         
 model = models.Sequential()
 model.add(layers.Dense(256, activation='relu', input_dim=np_input))
@@ -61,16 +57,32 @@ model.compile(loss='mse', optimizer='adam')
 
 history = model.fit(train, target, validation_split=0.2, batch_size=32, epochs=nb_epochs)
 
+# test actual error in angle over training data
+
+train_out = model.predict(train)
+err = (train_out - target)
+var = np.var(err)
+std = np.std(err)
+print("rect coord var: %f std: %f" % (var,std))
+
+err_angle = np.arctan2(np.sin(err[:,0]),np.cos(err[:,1]))
+var = np.var(err_angle)
+std = np.std(err_angle)
+print("angle var: %f std: %f" % (var,std))
+
 plot_en = 1;
 if plot_en:
     plt.figure(1)
-    plt.plot(history.history['loss'])
-    plt.plot(history.history['val_loss'])
-    plt.title('model loss')
+    plt.plot(10*np.log10(history.history['loss']))
+    plt.plot(10*np.log10(history.history['val_loss']))
+    plt.title('model loss (dB)')
     plt.xlabel('epoch')
     plt.legend(['train', 'valid'], loc='upper right')
+ 
+    plt.figure(2)
+    plt.hist(err_angle)
     plt.show()
-
+   
     #plt.figure(2)
     #plt.hist(target)
     #plt.show()
