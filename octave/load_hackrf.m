c64293a2e9f53d7e1fc2ff43e54d75af7d2ccc08
David <david@rowetel.com>
2019-10-04 16:38:00 +0930

    Revert "optional length param in load_hackrf"
    This reverts commit 831e57c777e054198eb6f65e73748d3d8bd7dd55.

--- octave/load_hackrf.m
+++ octave/load_hackrf.m
@@ -2,13 +2,9 @@
 %
 % David Rowe Oct 2015
 
-function s = load_hackrf(fn, n)
+function s = load_hackrf(fn)
   fs = fopen(fn,"rb");
-  if nargin == 1
-    iq = fread(fs,Inf,"schar");
-  else
-    iq = fread(fs,2*n,"schar");
-  end  
+  iq = fread(fs,Inf,"schar");
   fclose(fs);
   l = length(iq);
   s =  iq(1:2:l) + j*iq(2:2:l);
