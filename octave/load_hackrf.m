831e57c777e054198eb6f65e73748d3d8bd7dd55
David <david@rowetel.com>
2019-10-04 16:35:40 +0930

    optional length param in load_hackrf

--- octave/load_hackrf.m
+++ octave/load_hackrf.m
@@ -2,9 +2,13 @@
 %
 % David Rowe Oct 2015
 
-function s = load_hackrf(fn)
+function s = load_hackrf(fn, n)
   fs = fopen(fn,"rb");
-  iq = fread(fs,Inf,"schar");
+  if nargin == 1
+    iq = fread(fs,Inf,"schar");
+  else
+    iq = fread(fs,2*n,"schar");
+  end  
   fclose(fs);
   l = length(iq);
   s =  iq(1:2:l) + j*iq(2:2:l);
