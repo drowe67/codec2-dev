d88abe17453612255f5710c87b4fcd2f2185fe7a
David <david@rowetel.com>
2020-05-20 06:58:56 +0930

    unused verbose outputs

--- src/freedv_2020.c
+++ src/freedv_2020.c
@@ -198,7 +198,7 @@ void freedv_comptx_2020(struct freedv *f, COMP mod_out[]) {
 
 #ifdef __LPCNET__
 int freedv_comprx_2020(struct freedv *f, COMP demod_in[]) {
-    int   i, j, nout, k;
+    int   i, j, k;
     int   n_ascii;
     char  ascii_out;
     struct OFDM *ofdm = f->ofdm;
@@ -358,11 +358,11 @@ int freedv_comprx_2020(struct freedv *f, COMP demod_in[]) {
     ofdm_sync_state_machine(ofdm, rx_uw);
 
     if ((f->verbose && (ofdm->last_sync_state == search)) || (f->verbose == 2)) {
-        fprintf(stderr, "%3d st: %-6s euw: %2d %1d f: %5.1f pbw: %d snr: %4.1f %2d eraw: %3d ecdd: %3d iter: %3d pcc: %3d rxst: %d, nout: %4d\n",
+        fprintf(stderr, "%3d st: %-6s euw: %2d %1d f: %5.1f pbw: %d snr: %4.1f %2d eraw: %3d ecdd: %3d iter: %3d pcc: %3d rxst: %d\n",
                 f->frames++, ofdm_statemode[ofdm->last_sync_state], ofdm->uw_errors, ofdm->sync_counter, 
 		(double)ofdm->foff_est_hz, ofdm->phase_est_bandwidth,
                 f->snr_est, ofdm->frame_count_interleaver,
-                Nerrs_raw, Nerrs_coded, iter, parityCheckCount, rx_status, nout);
+                Nerrs_raw, Nerrs_coded, iter, parityCheckCount, rx_status);
     }
         
     return rx_status;
--- src/freedv_700.c
+++ src/freedv_700.c
@@ -485,7 +485,7 @@ int freedv_comprx_700c(struct freedv *f, COMP demod_in_8kHz[]) {
 
 int freedv_comp_short_rx_700d(struct freedv *f, void *demod_in_8kHz, int demod_in_is_short, float gain) {
     int   bits_per_codec_frame, bytes_per_codec_frame;
-    int   i, j, bit, byte, nout, k;
+    int   i, j, bit, byte, k;
     int   n_ascii;
     char  ascii_out;
     int   frames;
@@ -663,11 +663,11 @@ int freedv_comp_short_rx_700d(struct freedv *f, void *demod_in_8kHz, int demod_i
     ofdm_sync_state_machine(ofdm, rx_uw);
    
     if ((f->verbose && (ofdm->last_sync_state == search)) || (f->verbose == 2)) {
-        fprintf(stderr, "%3d nin: %4d st: %-6s euw: %2d %1d f: %5.1f phbw: %d snr: %4.1f %2d eraw: %3d ecdd: %3d iter: %3d pcc: %3d rxst: %d, nout: %4d\n",
+        fprintf(stderr, "%3d nin: %4d st: %-6s euw: %2d %1d f: %5.1f phbw: %d snr: %4.1f %2d eraw: %3d ecdd: %3d iter: %3d pcc: %3d rxst: %d\n",
                 f->frames++, ofdm->nin, ofdm_statemode[ofdm->last_sync_state], ofdm->uw_errors, ofdm->sync_counter, 
 		(double)ofdm->foff_est_hz, ofdm->phase_est_bandwidth,
                 f->snr_est, ofdm->frame_count_interleaver,
-                Nerrs_raw, Nerrs_coded, iter, parityCheckCount, rx_status, nout);
+                Nerrs_raw, Nerrs_coded, iter, parityCheckCount, rx_status);
     }
     
     return rx_status;
