47aaa99b87e5913a0af97b79d3762eaa7fe63d9b
David <david@rowetel.com>
2020-06-13 12:55:45 +0930

    first 2FSK LDPC frame decoding

--- octave/fsk_llr.m
+++ octave/fsk_llr.m
@@ -6,13 +6,16 @@
   TODO
   [X] Hard decision 4FSK modem simulation
   [X] single point AWGn simulation
-  [ ] SD outputs
-  [ ] histogram
-  [ ] modify for frames of LDPC codec size
-  [ ] LDPC codec with naive SD
+  [X] filter outputs
+  [X] histogram
+  [X] modify for frames of LDPC codec size
+  [X] LDPC codec with naive SD
 #}
 
 1;
+ldpc;
+
+% single Eb/No point simulation
 
 function [rx_filt rx_bits] = run_single(tx_bits, M=4, EbNodB=100)
   bps = log2(M);  % bits per symbol
@@ -69,26 +72,85 @@ function [rx_filt rx_bits] = run_single(tx_bits, M=4, EbNodB=100)
 
   Nerrors = sum(xor(tx_bits, rx_bits));
   ber = Nerrors/Nbits;
-  printf("EbNodB: %4.1f  Nerrors: %d BER: %1.3f\n", EbNodB, Nerrors, ber);
+  printf("EbNodB: %4.1f  Uncoded Nerrors: %d BER: %1.3f\n", EbNodB, Nerrors, ber);
 endfunction
 
-function plot_hist(Nbits,EbNodB)
-  Nbits = 10000; M=4;
+
+% Plot histograms of Rx filter outputs
+
+function plot_hist(Nbits=10000,EbNodB=2)
+  M=4;
   tx_bits = ones(1,Nbits);
-  rx_filt = run_single(tx_bits,M,EbNodB=6);
-  figure(1); clf;
+  rx_filt = run_single(tx_bits,M,EbNodB);
+  figure(1); clf; X = 0:31;
   for m=1:4
     subplot(2,2,m);
-    hist(rx_filt(:,m),25);
+    H=hist(rx_filt(:,m),X);
+    plot(X,H);
   end
 endfunction
+
+
+% 2FSK SD -> LLR mapping that we used for Wenet SSTV system
+
+function llr = sd_to_llr(sd)
+    sd = sd / mean(abs(sd));
+    x = sd - sign(sd);
+    sumsq = sum(x.^2);
+    summ = sum(x);
+    mn = summ/length(sd);
+    estvar = sumsq/length(sd) - mn*mn; 
+    estEsN0 = 1/(2* estvar + 1E-3); 
+    llr = 4 * estEsN0 * sd;
+endfunction
+
+
+% single point LDPC encoded frame simulation
+% Note: ~/cml/matCreateConstellation.m has some support for FSK
+
+function run_single_ldpc(Nbits=256,EbNodB=10)
+  M=2;
+  bps = 1; modulation = 'FSK'; mod_order=2; mapping = 'gray'; decoder_type = 0; max_iterations = 100;
+  load H_256_768_22.txt
+  code_param = ldpc_init_user(H_256_768_22, modulation, mod_order, mapping);
+  Nframes = floor(Nbits/code_param.data_bits_per_frame);
+  Nbits = Nframes*code_param.data_bits_per_frame;
+
+  % Encoder
+  data_bits = round(rand(1,Nbits));
+  tx_bits = [];
+  for f=1:Nframes;
+    codeword_bits = LdpcEncode(data_bits, code_param.H_rows, code_param.P_matrix);
+    tx_bits = [tx_bits codeword_bits];
+  end  
+  rx_filt = run_single(tx_bits,M,EbNodB);
+
+  Nerrors = 0;
+  % Decoder
+  sd = rx_filt(:,1) - rx_filt(:,2);
+  llr = sd_to_llr(sd)';
+  
+  [x_hat, PCcnt] = MpDecode(llr, code_param.H_rows, code_param.H_cols, ...
+                            max_iterations, decoder_type, 1, 1);         
+  Niters = sum(PCcnt!=0);
+  detected_data = x_hat(Niters,:);
+  Nerrors += sum(xor(data_bits, detected_data(1:length(data_bits))));
+  ber = Nerrors/Nbits;
+  printf("EbNodB: %4.1f  Coded   Nerrors: %d BER: %1.3f\n", EbNodB, Nerrors, ber);
+endfunction
+
+% Choose what you would like to run here --------------------------------------------------
 
 rand('seed',1);
 randn('seed',1);
 format short
 more off
+init_cml('~/cml/');
 
+#{
 % Eb/No = 6dB test pount should be about BER = 0.0157
 Nbits = 10000; tx_bits = round(rand(1,Nbits)); run_single(tx_bits,4,6);
 
 plot_hist;
+#}
+run_single_ldpc
