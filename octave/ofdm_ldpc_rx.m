5625516ab826d4ca61d259b6a5eccb4038dfbda6
David <david@rowetel.com>
2020-06-25 07:34:14 +0930

    using channel_est output

--- octave/ofdm_ldpc_rx.m
+++ octave/ofdm_ldpc_rx.m
@@ -54,7 +54,7 @@ function time_to_sync = ofdm_ldpc_rx(filename, mode="700D", error_pattern_filena
   % init logs and BER stats
 
   rx_bits = []; rx_np_log = []; timing_est_log = []; delta_t_log = []; foff_est_hz_log = [];
-  phase_est_pilot_log = [];
+  channel_est_pilot_log = [];
   sig_var_log = []; noise_var_log = [];
   Terrs = Tbits = Terrs_coded = Tbits_coded = Perrs_coded = 0;
   Nerrs_coded_log = Nerrs_log = [];
@@ -104,7 +104,7 @@ function time_to_sync = ofdm_ldpc_rx(filename, mode="700D", error_pattern_filena
       % accumulate a buffer of data symbols for this packet
       rx_syms(1:end-Nsymsperframe) = rx_syms(Nsymsperframe+1:end);
       rx_amps(1:end-Nsymsperframe) = rx_amps(Nsymsperframe+1:end);
-      [states rx_bits aphase_est_pilot_log arx_np arx_amp] = ofdm_demod(states, rxbuf_in);
+      [states rx_bits achannel_est_pilot_log arx_np arx_amp] = ofdm_demod(states, rxbuf_in);
       rx_syms(end-Nsymsperframe+1:end) = arx_np;
       rx_amps(end-Nsymsperframe+1:end) = arx_amp;
 
@@ -159,7 +159,7 @@ function time_to_sync = ofdm_ldpc_rx(filename, mode="700D", error_pattern_filena
       timing_est_log = [timing_est_log states.timing_est];
       delta_t_log = [delta_t_log states.delta_t];
       foff_est_hz_log = [foff_est_hz_log states.foff_est_hz];
-      phase_est_pilot_log = [phase_est_pilot_log; aphase_est_pilot_log];
+      channel_est_pilot_log = [channel_est_pilot_log; achannel_est_pilot_log];
       sig_var_log = [sig_var_log states.sig_var];
       noise_var_log = [noise_var_log states.noise_var];
       frame_count++;
@@ -207,20 +207,25 @@ function time_to_sync = ofdm_ldpc_rx(filename, mode="700D", error_pattern_filena
   printf("Raw BER..: %5.4f Tbits: %5d Terrs: %5d\n", Terrs/(Tbits+1E-12), Tbits, Terrs);
   printf("Coded BER: %5.4f Tbits: %5d Terrs: %5d\n", Terrs_coded/(Tbits_coded+1E-12), Tbits_coded, Terrs_coded);
   printf("Coded PER: %5.4f Pckts: %5d Perrs: %5d\n", Perrs_coded/(packet_count+1E-12), packet_count, Perrs_coded);
-
+  
   if length(rx_np_log)
       figure(1); clf; 
-      plot(rx_np_log,'+');
-      mx = max(abs(rx_np_log));
+      plot(exp(j*pi/4)*rx_np_log(floor(end/4):floor(end-end/8)),'+');
+      mx = 2*max(abs(rx_np_log));
       axis([-mx mx -mx mx]);
       title('Scatter');
 
       figure(2); clf;
-      plot(phase_est_pilot_log,'g+', 'markersize', 5); 
+      plot(angle(channel_est_pilot_log),'g+', 'markersize', 5); 
       title('Phase est');
-      axis([1 length(phase_est_pilot_log) -pi pi]);
+      axis([1 length(channel_est_pilot_log) -pi pi]);
 
       figure(3); clf;
+      plot(abs(channel_est_pilot_log(:,2:Nc)),'g+', 'markersize', 5); 
+      title('Amp est');
+      axis([1 length(channel_est_pilot_log) -3 3]);  
+
+      figure(4); clf;
       subplot(211)
       stem(delta_t_log)
       title('delta t');
@@ -228,7 +233,7 @@ function time_to_sync = ofdm_ldpc_rx(filename, mode="700D", error_pattern_filena
       plot(timing_est_log);
       title('timing est');
 
-      figure(4); clf;
+      figure(5); clf;
       plot(foff_est_hz_log)
       mx = max(abs(foff_est_hz_log));
       axis([1 max(Nframes,2) -mx mx]);
@@ -237,7 +242,7 @@ function time_to_sync = ofdm_ldpc_rx(filename, mode="700D", error_pattern_filena
   end
    
   if length(Nerrs_log) > 1
-    figure(5); clf;
+    figure(6); clf;
     subplot(211)
     stem(Nerrs_log);
     title('Uncoded errrors/modem frame')
@@ -250,14 +255,14 @@ function time_to_sync = ofdm_ldpc_rx(filename, mode="700D", error_pattern_filena
     end
   end
   
-  figure(6); clf;
+  figure(7); clf;
   snr_estdB = 10*log10(sig_var_log) - 10*log10(noise_var_log) + 10*log10(Nc*Rs/3000);
   snr_smoothed_estdB = filter(0.1,[1 -0.9],snr_estdB);
   plot(snr_smoothed_estdB);
   title('Signal and Noise Power estimates');
   ylabel('SNR (dB)')
 
-  figure(7); clf; plot_specgram(rx);
+  figure(8); clf; plot_specgram(rx);
   if len_secs
     axis([0 len_secs 500 2500])
   end
