3f3a0f1222b85e00392456d8912529b6d8f92d27
David <david@rowetel.com>
2020-08-07 07:14:19 +0930

    building up BER est

--- octave/papr_test.m
+++ octave/papr_test.m
@@ -9,44 +9,81 @@
     [ ] heat map type scatter diagram
 #}
 
-M=160;   % length of one symbol
-Nc=16;    % number of carriers
-N=80000;  % how many samples to simulate
-frames = N/M
+M = 160;      % length of one symbol in samples
+Nc = 8;      % number of carriers
+Nsym = 1000;   % number of symbols to simulate
+bps  = 2;     % two bits per symbol for QPSK
 
-tx_phase = zeros(frames,Nc);
-tx_phase(:, 1:Nc/2) = pi/2*floor((rand(frames,Nc/2)*4));
-tx_phase(:, Nc/2+1:Nc) = pi - tx_phase(:,1:Nc/2);
+function symbol = qpsk_mod(two_bits)
+    two_bits_decimal = sum(two_bits .* [2 1]); 
+    switch(two_bits_decimal)
+        case (0) symbol =  1;
+        case (1) symbol =  j;
+        case (2) symbol = -j;
+        case (3) symbol = -1;
+    endswitch
+endfunction
+
+function two_bits = qpsk_demod(symbol)
+    bit0 = real(symbol*exp(j*pi/4)) < 0;
+    bit1 = imag(symbol*exp(j*pi/4)) < 0;
+    two_bits = [bit1 bit0];
+endfunction
+
+% generate a 2D array of QPSK symbols
+
+Nphases = 2^bps;
+tx_phases = pi/2*floor((rand(Nsym,Nc)*Nphases));
+tx_sym = exp(j*tx_phases);
 
 w = 2*pi/M;
 tx = [];
 
 % generate OFDM signal
-for f=1:frames
+
+for s=1:Nsym
   atx = zeros(1,M);
   for c=1:Nc
-    atx += exp(j*(0:M-1)*c*w)*exp(j*tx_phase(f,c));
+    atx += exp(j*(0:M-1)*c*w)*tx_sym(s,c);
   end
   tx = [tx atx];
 end
+Nsam = length(tx);
 
-% channel
+% AWGN channel
 
-rx = tx;
-rx(find(rx>10))=10;
+EbNodB = 4;
+EsNodB = EbNodB + 10*log10(bps);
+variance = M/(10^(EsNodB/10));
+noise = sqrt(variance/2)*randn(1,Nsam) + j*sqrt(variance/2)*randn(1,Nsam);
+rx = tx + noise;
+
 % demodulate
 
-rx_symb = zeros(frames,Nc);
-for f=1:frames
-  st = (f-1)*M+1; en = f*M;
+rx_sym = zeros(Nsym,Nc);
+for s=1:Nsym
+  st = (s-1)*M+1; en = s*M;
   for c=1:Nc
-   rx_symb(f,c) = sum(exp(-j*(0:M-1)*c*w) .* rx(st:en))/M;
+    rx_sym(s,c) = sum(exp(-j*(0:M-1)*c*w) .* rx(st:en))/M;
+  end
+end
+
+% count bit errors
+
+Tbits = Terrs = 0;
+for s=1:Nsym
+  for c=1:Nc
+    tx_bits = qpsk_demod(tx_sym(s,c));
+    rx_bits = qpsk_demod(rx_sym(s,c));
+    Tbits += bps;
+    Terrs += sum(xor(tx_bits,rx_bits));
   end
 end
 
 figure(1); clf;
 plot(abs(tx(1:10*M)))
-figure(2); clf; hist(abs(rx),25)
-figure(3); clf; plot(rx_symb,'+')
-printf("PAPR: %f dB\n", 20*log10(max(abs(rx))/mean(abs(rx))))
+figure(2); clf; hist(abs(tx),25)
+figure(3); clf; plot(rx_sym,'+'); axis([-2 2 -2 2]);
+papr = 20*log10(max(abs(tx))/mean(abs(tx)));
+printf("PAPR: %5.2f dB Tbits: %d Terrs: %d BER: %5.3f\n", papr, Tbits, Terrs, Terrs/Tbits)
   
