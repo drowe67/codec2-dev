89c1dfd42d08c61376ba477c5bd5e306cf1a3dac
David Rowe <david@rowetel.com>
2020-09-28 03:32:06 +0100

    C++ linkage for ldpc_codes.h

--- src/ldpc_codes.h
+++ src/ldpc_codes.h
@@ -7,6 +7,11 @@
 */
 
 #ifndef __LDPC_CODES__
+
+#ifdef __cplusplus
+  extern "C" {
+#endif
+
 #include "mpdecode_core.h"
 
 extern struct LDPC ldpc_codes[];
@@ -14,5 +19,9 @@ int ldpc_codes_num(void);
 void ldpc_codes_list();
 int ldpc_codes_find(char name[]);
 void ldpc_codes_setup(struct LDPC *ldpc, char name[]);
+
+#ifdef __cplusplus
+  }
+#endif
 
 #endif
