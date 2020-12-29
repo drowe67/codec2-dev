0bb13af9c2f4ef077032f0f1eb7d859ea0588cb7
drowe67 <david@rowetel.com>
2020-12-29 12:23:23 +1030

    updated plamp tool to help analyse compressed speech

--- octave/plamp.m
+++ octave/plamp.m
@@ -8,11 +8,7 @@ function plamp(samname, f, samname2)
 
   % switch some stuff off to unclutter display
 
-  plot_lsp = 0;
-  plot_snr = 0;
-  plot_vsnr = 0;
   plot_sw = 0;
-  plot_pw = 0;
 
   sn_name = strcat(samname,"_sn.txt");
   Sn = load(sn_name);
@@ -85,10 +81,10 @@ function plamp(samname, f, samname2)
     figure(1);
     clf;
     s = [ Sn(2*f-1,:) Sn(2*f,:) ];
-    plot(s);
+    plot(s,'b');
     if (file_in_path(".",model2_name))
       s2 = [ Sn2(2*f-1,:) Sn2(2*f,:) ];
-      hold on; plot(s2); hold off;
+      hold on; plot(s2,'r'); hold off;
     end
     axis([1 length(s) -30000 30000]);
 
@@ -96,50 +92,42 @@ function plamp(samname, f, samname2)
     Wo = model(f,1);
     L = model(f,2);
     Am = model(f,3:(L+2));
-    plot((1:L)*Wo*4000/pi, 20*log10(Am),";Am;+-r");
+    plot((1:L)*Wo*4000/pi, 20*log10(Am),";Am;+-b");
     axis([1 4000 -10 80]);
     hold on;
-    if plot_sw
-      plot((0:255)*4000/256, Sw(f,:),";Sw;");
-    end
+    if plot_sw; plot((0:255)*4000/256, Sw(f,:),";Sw;b"); end
 
     if (file_in_path(".",model2_name))
       Wo2 = model2(f,1);
       L2 = model2(f,2);
       Am2 = model2(f,3:(L2+2));
-      plot((1:L2)*Wo2*4000/pi, 20*log10(Am2),";Am2;+-g" );
-      plot((0:255)*4000/256, Sw2(f,:),";Sw2;");
+      plot((1:L2)*Wo2*4000/pi, 20*log10(Am2),";Am2;+-r" );
+      if plot_sw; plot((0:255)*4000/256, Sw2(f,:),";Sw2;r"); end
     endif
 
-    hold off;
+    hold off; grid minor;
 
     % interactive menu
 
-    printf("\rframe: %d  menu: n-next  b-back  p-png  q-quit", f);
+    printf("\rframe: %d  menu: n-next  b-back  p-png  s-plot_sw q-quit", f);
     fflush(stdout);
     k = kbhit();
-    if (k == 'n')
-      f = f + 1;
+    if k == 'n'; f = f + 1; endif
+    if k == 'b'; f = f - 1; endif
+    if k == 's'
+        if plot_sw; plot_sw = 0; else; plot_sw = 1; end
     endif
-    if (k == 'b')
-      f = f - 1;
-    endif
-
     % optional print to PNG
 
     if (k == 'p')
       figure(1);
       pngname = sprintf("%s_%d_sn.png",samname,f);
-      print(pngname, '-dpng', "-S500,500")
-      pngname = sprintf("%s_%d_sn_large.png",samname,f);
       print(pngname, '-dpng', "-S800,600")
 
       figure(2);
       pngname = sprintf("%s_%d_sw.png",samname,f);
-      print(pngname, '-dpng', "-S500,500")
-      pngname = sprintf("%s_%d_sw_large.png",samname,f);
-      print(pngname, '-dpng', "-S1200,800")
-    endif
+      print(pngname, '-dpng', "-S800,600")
+     endif
 
   until (k == 'q')
   printf("\n");
