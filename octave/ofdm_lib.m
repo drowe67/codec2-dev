ada47fda6e201ec27e5687c0d23c0f4544afc9ae
drowe67 <david@rowetel.com>
2021-01-01 08:27:45 +1030

    wip datac3

--- octave/ofdm_lib.m
+++ octave/ofdm_lib.m
@@ -303,10 +303,11 @@ function config = ofdm_init_mode(mode="700D")
     config.state_machine = "data";
     config.ftwindow_width = 32; config.amp_est_mode = 1; config.EsNodB = 10;
   elseif strcmp(mode,"datac3")
-    Ns=5; config.Np=11; Tcp = 0.006; Ts = 0.016; Nc = 9; config.data_mode = 1;
-    config.Ntxtbits = 0; config.Nuwbits = 24; config.bad_uw_errors = 5;
+    Ns=5; config.Np=11; Tcp = 0.006; Ts = 0.016; Nc = 28; config.data_mode = 1;
+    config.Ntxtbits = 0; config.Nuwbits = 48; config.bad_uw_errors = 10;
     config.ftwindow_width = 32; config.timing_mx_thresh = 0.30;
-    config.tx_uw = [1 1 0 0  1 0 1 0  1 1 1 1  0 0 0 0  1 1 1 1  0 0 0 0];
+    config.tx_uw = zeros(1,config.Nuwbits);
+    config.tx_uw(1:24) = [1 1 0 0  1 0 1 0  1 1 1 1  0 0 0 0  1 1 1 1  0 0 0 0];
     config.amp_est_mode = 1; config.EsNodB = 0;
     config.state_machine = "data";
   elseif strcmp(mode,"1")
