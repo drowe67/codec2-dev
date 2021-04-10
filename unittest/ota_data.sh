ccade1941f1d9ea42406f6e5aecd83aae688d207
David Rowe <david@rowetel.com>
2021-04-11 08:05:27 +0930

    spectrograms, and cli mode selection

--- unittest/ota_data.sh
+++ unittest/ota_data.sh
@@ -9,7 +9,6 @@
 # 3. Install Hamlib cli tools
 #
 # TODO:
-#  [ ] plot spectograms
 #  [ ] plot scatter diagram
 #  [ ] SNR output
 #  [ ] timestamps
@@ -23,14 +22,15 @@ freq_kHz="7177"
 mode="lsb"
 tx_only=0
 Nbursts=10
+mode="datac0"
 
 function print_help {
     echo
     echo "Automated Over The Air (OTA) data test for FreeDV OFDM HF data modems"
     echo
-    echo "  usage ./ota_data.sh [-f freq_kHz] [-m mode] [-t] [-b Nbursts] kiwi_url"
+    echo "  usage ./ota_data.sh [-f freq_kHz] [-t] [-b Nbursts] kiwi_url"
     echo
-    echo "    -m mode  usb|lsb"
+    echo "    -m mode  datac0|datac1|datac3"
     echo "    -t       Tx only, useful for manually observing SDRs which block multiple sessions from one IP"
     echo
     exit
@@ -71,19 +71,22 @@ esac
 done
 set -- "${POSITIONAL[@]}" # restore positional parameters
 
-if [ $# -lt 1 ]; then
-    print_help
+if [ $tx_only -eq 0 ]; then
+    if [ $# -lt 1 ]; then
+        print_help
+    fi
+    kiwi_url="$1"	
 fi
-kiwi_url="$1"	
 
 # create test Tx file
-freedv_data_raw_tx --framesperburst 1 --bursts ${Nbursts} --testframes ${Nbursts} datac0 /dev/zero test_datac0.raw
+freedv_data_raw_tx --framesperburst 1 --bursts ${Nbursts} --testframes ${Nbursts} ${mode} /dev/zero test_datac0.raw
 
 if [ $tx_only -eq 0 ]; then
     # start recording from remote kiwisdr
     kiwi_stdout=$(mktemp)
     kiwi_rxfile=$(mktemp)
-    kiwirecorder.py -s $kiwi_url -f $freq_kHz -m $mode -r 8000 --filename=${kiwi_rxfile} --time-limit=60 >$kiwi_stdout &
+    usb_lsb=$(python3 -c "print('usb') if ${freq_kHz} >= 10000 else print('lsb')")
+    kiwirecorder.py -s $kiwi_url -f $freq_kHz -m ${usb_lsb} -r 8000 --filename=${kiwi_rxfile} --time-limit=60 >$kiwi_stdout &
     kiwi_pid=$!
 
     # wait for kiwi to start recording
@@ -100,9 +103,15 @@ aplay --device="plughw:CARD=CODEC,DEV=0" -f S16_LE test_datac0.raw
 echo "\\set_ptt 0" | rigctl -m 361 -r /dev/ttyUSB0
 
 if [ $tx_only -eq 0 ]; then
+    sleep 2
     kill ${kiwi_pid}
     wait ${kiwi_pid}
 
+    # generate spectrogram
+    echo "pkg load signal; \
+          s=load_raw('${kiwi_rxfile}.wav'); \
+          plot_specgram(s, 8000, 500, 2500); print('spec.png', '-dpng'); \
+          quit" | octave-cli -p ../octave -qf
     # attempt to demodulate
-    freedv_data_raw_rx --framesperburst 1 --testframes datac0 ${kiwi_rxfile}.wav /dev/null
+    freedv_data_raw_rx --framesperburst 1 --testframes ${mode} ${kiwi_rxfile}.wav /dev/null
 fi
