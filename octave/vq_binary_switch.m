23a4d7b50f9de5705f1b510192288e8c745aa765
drowe67 <david@rowetel.com>
2021-09-04 10:39:19 +0930

    prototyped more efficient version in Octave

--- octave/vq_binary_switch.m
+++ octave/vq_binary_switch.m
@@ -8,6 +8,16 @@
 
 1;
 
+% returns indexes of hamming distance 1 neighbours
+function index_neighbours = distance_one_neighbours(N,k)
+  log2N = log2(N);
+  index_neighbours = [];
+  for b=0:log2N-1
+    index_neighbour = bitxor(k-1,2.^b) + 1;
+    index_neighbours = [index_neighbours index_neighbour];
+  end
+end
+
 % equation (33) of [1], for hamming distance 1
 function c = cost_of_distance_one(vq, prob, k, verbose=0)
   [N K] = size(vq);
@@ -25,16 +35,20 @@ function c = cost_of_distance_one(vq, prob, k, verbose=0)
 endfunction
 
 % equation (39) of [1]
-function d = distortion_of_current_mapping(vq, prob)
+function d = distortion_of_current_mapping(vq, prob, verbose=0)
   [N K] = size(vq);
   
   d = 0;
   for k=1:N
-    d += cost_of_distance_one(vq, prob, k);
+    c = cost_of_distance_one(vq, prob, k);
+    d += c;
+    if verbose
+      printf("k: %2d c: %f d: %f\n", k, c, d);
+    end
   end
 endfunction
 
-function [vq distortion] = binary_switching(vq, prob, max_iteration)
+function [vq distortion] = binary_switching(vq, prob, max_iteration, fast_en=1)
   [N K] = size(vq);
   iteration = 0;
   i = 1;
@@ -56,20 +70,40 @@ function [vq distortion] = binary_switching(vq, prob, max_iteration)
     for j=2:N
       % we can't switch with ourself
       if j != A(i)
-        distortion1 = distortion_of_current_mapping(vq, prob);
+        if fast_en
+          delta = -cost_of_distance_one(vq, prob, A(i)) - cost_of_distance_one(vq, prob, j);
+	  n1 = [distance_one_neighbours(N,A(i)) distance_one_neighbours(N,j)];
+	  n1(n1 == A(i)) = [];
+	  n1(n1 == j) = [];
+	  for l=1:length(n1)
+	    delta -= cost_of_distance_one(vq, prob, n1(l));
+	  end
+        else
+	  distortion1 = distortion_of_current_mapping(vq, prob);
+	end
+	
         % switch vq entries A(i) and j
 	tmp = vq(A(i),:);
 	vq(A(i),:) = vq(j,:);
 	vq(j,:) = tmp;
+		
+        if fast_en
+	  delta += cost_of_distance_one(vq, prob, A(i)) + cost_of_distance_one(vq, prob, j);
+	  for l=1:length(n1)
+	    delta += cost_of_distance_one(vq, prob, n1(l));
+	  end
+	else
+	  distortion2 = distortion_of_current_mapping(vq, prob);
+          delta = distortion2 - distortion1;
+	end
 	
-	distortion2 = distortion_of_current_mapping(vq, prob);
-        delta = distortion2 - distortion1;
 	if delta < 0
 	  if abs(delta) > best_delta
 	    best_delta = abs(delta);
 	    best_j = j;
 	  end
 	end
+	
 	% unswitch
 	tmp = vq(A(i),:);
 	vq(A(i),:) = vq(j,:);
@@ -106,6 +140,17 @@ function [vq distortion] = binary_switching(vq, prob, max_iteration)
 
 endfunction
 
+% return indexes of hamming distance one vectors
+function ind = neighbour_indexes(vq, k)
+  [N K] = size(vq);
+  log2N = log2(N);
+  ind = [];
+  for b=0:log2N-1
+    index_neighbour = bitxor(k-1,2.^b) + 1;
+    ind = [ind index_neighbour];
+  end
+endfunction
+
 function test_binary_switch
   vq1 = [1 1; -1 1; -1 -1; 1 -1];
   %f=fopen("vq1.f32","wb"); fwrite(f, vq1, 'float32'); fclose(f);
@@ -118,41 +163,43 @@ function test_binary_switch
   printf("test_binary_switch OK!\n");
 endfunction
 
-% return indexes of hamming distance one vectors
-function ind = neighbour_indexes(vq, k)
-  [N K] = size(vq);
-  log2N = log2(N);
-  ind = [];
-  for b=0:log2N-1
-    index_neighbour = bitxor(k-1,2.^b) + 1;
-    ind = [ind index_neighbour];
-  end
+function test_fast
+  N=16;     % Number of VQ codebook vectors
+  K=2;      % Vector length
+  Ntrain=10000;
+
+  training_data = randn(Ntrain,K);
+  [idx vq1] = kmeans(training_data, N);
+  [vq2 distortion] = binary_switching(vq1, [1 ones(1,N-1)], 1000, fast_en = 0);
+  [vq3 distortion] = binary_switching(vq1, [1 ones(1,N-1)], 1000, fast_en = 1);
+  assert(vq2 == vq3);  
+  printf("test_fast OK!\n");
 endfunction
 
+function demo
+  N=16;     % Number of VQ codebook vectors
+  K=2;      % Vector length
+  Ntrain=10000;
+  training_data = randn(Ntrain,K);
+  [idx vq1] = kmeans(training_data, N);
+  [vq2 distortion] = binary_switching(vq1, [1 ones(1,N-1)], 1000, 1);
+
+  figure(1); clf; plot(training_data(:,1), training_data(:,2),'+');
+  hold on;
+  plot(vq1(:,1), vq1(:,2),'og','linewidth', 2);
+  plot(vq2(:,1), vq2(:,2),'or','linewidth', 2);
+
+  % plot hamming distance 1 neighbours
+  k = 1;
+  ind = neighbour_indexes(vq2, k);
+  for i=1:length(ind)
+    plot([vq2(k,1) vq2(ind(i),1)],[vq2(k,2) vq2(ind(i),2)],'r-','linewidth', 2);
+  end
+  hold off;
+endfunction
+
+pkg load statistics
 %test_binary_switch;
-
-randn('seed',1);
-N=16;     % Number of VQ codebook vectors
-K=2;      % Vector length
-
-Ntrain=10000;
-
-training_data = randn(Ntrain,K);
-[idx vq1] = kmeans(training_data, N);
-[vq2 distortion] = binary_switching(vq1, [1 ones(1,N-1)], 1000);
-
-figure(1); clf; plot(training_data(:,1), training_data(:,2),'+');
-hold on;
-plot(vq1(:,1), vq1(:,2),'og','linewidth', 2);
-plot(vq2(:,1), vq2(:,2),'or','linewidth', 2);
-
-% plot hamming distance 1 neighbours
-k = 1;
-ind = neighbour_indexes(vq2, k);
-for i=1:length(ind)
-  plot([vq2(k,1) vq2(ind(i),1)],[vq2(k,2) vq2(ind(i),2)],'r-','linewidth', 2);
-end
-
-hold off;
-
+test_fast;
+%demo
 
