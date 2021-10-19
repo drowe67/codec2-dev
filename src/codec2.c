233a6843bf94dbd0e13a0bd8505f75d3707f3068
Steve <coupaydeville@gmail.com>
2021-10-19 14:28:29 -0500

    Update codec2.c
    Compute variance on the fly

--- src/codec2.c
+++ src/codec2.c
@@ -206,9 +206,7 @@ struct CODEC2 * codec2_create(int mode)
 
     c2->smoothing = 0;
 
-    c2->se = 0.0f;
     c2->variance = 0.0f;
-    c2->nse = 0;
     
     c2->user_rate_K_vec_no_mean_ = NULL;
     c2->post_filter_en = 1;
@@ -1582,7 +1580,7 @@ void codec2_encode_700c(struct CODEC2 *c2, unsigned char * bits, short speech[])
                              &mean,
                              rate_K_vec_no_mean,
                              rate_K_vec_no_mean_, &c2->se, c2->eq, c2->eq_en);
-    c2->nse += K;
+
     c2->variance += (c2->se / (float) K);
 	
 #ifndef CORTEX_M4
@@ -2305,6 +2303,4 @@ void codec2_700c_post_filter(struct CODEC2 *codec2_state, int en) {
 void codec2_700c_eq(struct CODEC2 *codec2_state, int en) {
     codec2_state->eq_en = en;
     codec2_state->variance = 0.0f;
-    codec2_state->se = 0.0f;
-    codec2_state->nse = 0;
 }
