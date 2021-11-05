dee715b3c0db16e7997b61c23e42e2bea0fb02f8
srsampson <coupaydeville@gmail.com>
2021-11-05 13:26:33 -0500

    Add missing stdlib include

--- src/framer.c
+++ src/framer.c
@@ -29,6 +29,7 @@
 
 #include <assert.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include "fsk.h"
 
--- src/fsk_get_test_bits.c
+++ src/fsk_get_test_bits.c
@@ -28,7 +28,9 @@
 
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <alloca.h>
 #include "fsk.h"
 
 #define TEST_FRAME_SIZE 100  /* arbitrary choice, repeats after this
