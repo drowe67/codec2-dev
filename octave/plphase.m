2b5bfe3d57bd0c2399bbc005494cf4d605b7c428
drowe67 <david@rowetel.com>
2021-11-07 08:39:48 +1030

    ability to toggle between original and phase0

--- octave/plphase.m
+++ octave/plphase.m
@@ -10,15 +10,23 @@
   1/ Contrived speech-like test signal:
   
   $ cd codec2/build_linux
-  $ ./misc/timpulse --f0 100 --n0 1 --filter | ./src/c2sim - --modelout - | ./misc/est_n0 > imp_n0.txt
+  $ ./misc/timpulse --f0 100 --n0 1 --filter | ./src/c2sim - --rateK --modelout - | ./misc/est_n0 > imp_n0.txt
   $ ./misc/timpulse --f0 100 --n0 1 --filter | ./src/c2sim - --rateK --phase0 --dump imp
   octave:> plphase("../build_linux/imp", 20)
 
   2/ A real speech file:
   
-  $ cd codec2/build_linux
-  $ ./src/c2sim ../raw/hts1a.raw --ratek --phase0 --dump hts1a --modelout - | ./misc/est_n0 > hts1a_n0.txt
-  octave:> plphase("../build_linux/hts1a", 20)
+  Generate n0 estimates for original and phase0:
+
+    $ cd codec2/build_linux
+    $ ./src/c2sim ../raw/hts1a.raw --rateK --modelout - | ./misc/est_n0 > hts1a_n0.txt
+    $ ./src/c2sim ../raw/hts1a.raw --rateK --phase0 --modelout - | ./misc/est_n0 > hts1a_phase0_n0.txt
+
+  Generate "dump files":
+  
+    $ ./src/c2sim ../raw/hts1a.raw --rateK --phase0 --dump hts1a
+    
+  octave:> plphase("../build_linux/hts1a", 44)
 #}
 
 function plphase(samname, f)
@@ -38,6 +46,10 @@ function plphase(samname, f)
   if (file_in_path(".",n0_name))
     n0 = load(n0_name);
   endif
+  phase0_n0_name = strcat(samname,"_phase0_n0.txt");
+  if (file_in_path(".",phase0_n0_name))
+    phase0_n0 = load(phase0_n0_name);
+  endif
   
   phase_name = strcat(samname,"_phase.txt");
   if (file_in_path(".",phase_name))
@@ -49,7 +61,7 @@ function plphase(samname, f)
     phase_ = load(phase_name_);
   endif
 
-  k = ' '; plot_group_delay=1; Pms = 6;
+  k = ' '; plot_group_delay=0; Pms = 1; plot_orig=1;
   do
     figure(1); clf;
     s = [ Sn(2*f-1,:) Sn(2*f,:) ];
@@ -69,9 +81,15 @@ function plphase(samname, f)
     hold on;
  
     % estimate group and phase delay
-    
-    phase_rect = exp(j*phase(f,1:L));
-    phase_linear = exp(j*(1:L)*Wo*n0(f));
+
+    if plot_orig
+      phase_f = phase(f,1:L);
+      phase_linear = exp(j*(1:L)*Wo*n0(f));
+    else
+      phase_f = phase_(f,1:L);
+      phase_linear = exp(j*(1:L)*Wo*phase0_n0(f));
+    end
+    phase_rect = exp(j*phase_f);
     phase_centred_rect = phase_rect .* conj(phase_linear);
     phase_centred = angle(phase_centred_rect);
     group_delay = [0 -(angle(phase_centred_rect(2:L).*conj(phase_centred_rect(1:L-1)))/Wo)*1000/Fs];
@@ -85,7 +103,6 @@ function plphase(samname, f)
     end
     hold off;
     axis(ax(1), [1 Fs2 -10 80]);
-    Pms
     axis(ax(2), [1 Fs2 -Pms Pms]);
     xlabel('Frequency (Hz)');
     ylabel(ax(1),'Amplitude (dB)');
@@ -101,25 +118,31 @@ function plphase(samname, f)
        png(pngname);
     endif
 
-    if (file_in_path(".",phase_name))
-      figure(3);
-      subplot(211);
-      plot((1:L)*Wo*Fs2/pi, phase(f,1:L), "-o;phase;");
-      hold on;
-      plot((1:L)*Wo*Fs2/pi, phase_centred, "-og;phase centered;"); axis([0 Fs2 -pi pi]);
-      hold off;
-      subplot(212);
-      plotyy(x_group, group_delay, x_phase, phase_delay);
-      
-      if (k == 'p')
-        pngname = sprintf("%s_%d_phase",samname,f);
-        png(pngname);
-      endif
+    figure(3); clf;
+    subplot(211);
+    plot((1:L)*Wo*Fs2/pi, phase_f, "-o;phase;");
+    hold on;
+    plot((1:L)*Wo*Fs2/pi, phase_centred, "-og;phase centered;"); axis([0 Fs2 -pi pi]);
+    hold off;
+    subplot(212);
+    if plot_group_delay
+      plot(x_group, group_delay, "-o;group;");
+      axis([1 Fs2 -Pms Pms]);
+    else
+      plot(x_phase, phase_delay, "-o;phase;");
+      axis([1 Fs2 -1 1]);
+    end
+    
+    if (k == 'p')
+      pngname = sprintf("%s_%d_phase",samname,f);
+      png(pngname);
     endif
-
+ 
     % interactive menu
 
-    printf("\rframe: %d  menu: n-next  b-back  g-group/phase p-png  q-quit ", f);
+    if plot_group_delay; s1="[group dly]/phase dly"; else s1="group dly/[phase dly]"; end
+    if plot_orig; s2="[orig]/phase0"; else s2="orig/[phase0]"; end
+    printf("\rframe: %d  menu: n-next  b-back  g-%s o-%s p-png  q-quit ", f, s1,s2);
     fflush(stdout);
     k = kbhit();
     if (k == 'n')
@@ -135,6 +158,13 @@ function plphase(samname, f)
       else
         plot_group_delay = 1;
 	Pms=6;
+      end
+    endif
+    if (k == 'o')
+      if plot_orig
+        plot_orig = 0;
+      else
+        plot_orig = 1;
       end
     endif
 
