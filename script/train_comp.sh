8395f65e8e82d6ec7eb5d90dc612f8688db66075
drowe67 <david@rowetel.com>
2022-05-13 11:53:36 +0930

    clean up train_comp, just nm2 and two band comp candidates now

--- script/train_comp.sh
+++ script/train_comp.sh
@@ -4,6 +4,8 @@
 #
 # Training and testing rateK Vector Quantisers (VQ) for Codec 2, using
 # audio compression and AGC.
+#
+# Results May 2022: nm2 > two band comp
 
 TRAIN=~/Downloads/train2.spc
 CODEC2_PATH=$HOME/codec2
@@ -12,21 +14,9 @@ K=20
 Kst=2
 Ken=18
 
-# compressor with AGC
-function compress() {
-  fullfile=$1
-  filename=$(basename -- "$fullfile")
-  extension="${filename##*.}"
-  filename="${filename%.*}"
-  tmp=$(mktemp)
-  # SSB filter -> peak after filter (removes F0)
-  ch $fullfile - 2>/dev/null | ch - /dev/null 2>$tmp
-  peak=$(cat $tmp | grep peak | tr -s ' ' | cut -f3 -d' ')
-  gain=$(python -c "gain=32767.0/${peak}; print(\"%3.1f\" % gain)")
-  ch $fullfile ${filename}_comp.s16 --gain $gain --clip 16384 2>$tmp
-  papr=$(cat $tmp | grep peak | tr -s ' ' | cut -f7 -d' ')
-  printf "PAPR: %5.2f\n" $papr
-}
+# ---------------------------------------------------------------------------------
+# Utility Functions ---------------------------------------------------------------
+# ---------------------------------------------------------------------------------
 
 # Just AGC - 200 Hz HPF and normalise peak levels
 function agc() {
@@ -43,136 +33,10 @@ function agc() {
   printf "PAPR: %5.2f\n" $papr
 }
 
-# train - AGC, SSB filter, Hilbert clipper compressed, subset VQ
-function train_compressed() {
-  fullfile=$TRAIN
-  filename=$(basename -- "$fullfile")
-  extension="${filename##*.}"
-  filename="${filename%.*}"
+# ---------------------------------------------------------------------------------
+# AGC, remove mean based on mean of Kst to Ken (so mean needs to be quantised separately)
+# ---------------------------------------------------------------------------------
 
-  # initial step applies SSB filter
-  ch ${fullfile} - | ch - ${filename}_comp.s16 --clip 16384 --clipmin 10 --gain 2.0
-  c2sim ${filename}_comp.s16 --rateK --rateK_mean_min 10 --rateK_mean_max 40 --rateKout ${filename}_comp.f32
-  vqtrain ${filename}_comp.f32 $K 512 vq_stage1.f32 -s 1e-3 --st $Kst --en $Ken -r stage2_in.f32
-  vqtrain stage2_in.f32 $K 512 vq_stage2.f32 -s 1e-3 --st $Kst --en $Ken -r stage3_in.f32
-  vqtrain stage3_in.f32 $K 512 vq_stage3.f32 -s 1e-3 --st $Kst --en $Ken
-}
-
-function listen_compressed() {
-  fullfile=$1
-  filename=$(basename -- "$fullfile")
-  extension="${filename##*.}"
-  filename="${filename%.*}"
-
-  o=agc_ssb_comp
-  mkdir -p $o
-  compress ${fullfile}
-  c2sim ${filename}_comp.s16 --rateK --rateK_mean_min 10 --rateK_mean_max 40 --rateKout ${filename}.f32 \
-        --phase0 --postfilter -o - | sox -t .s16 -r 8000 -c 1 - ${o}/${filename}_ratek.wav
-  cat ${filename}.f32 | vq_mbest --st $Kst --en $Ken -k $K -q vq_stage1.f32 > ${filename}_test.f32
-  c2sim ${filename}_comp.s16 --rateK --rateK_mean_min 10 --rateK_mean_max 40 --rateKin ${filename}_test.f32  \
-        --phase0 --postfilter -o - | sox -t .s16 -r 8000 -c 1 - ${o}/${filename}_comp_vq.wav
-  cat ${filename}.f32 | vq_mbest --st $Kst --en $Ken -k $K -q vq_stage1.f32,vq_stage2.f32 --mbest 5 \
-      > ${filename}_test.f32
-  c2sim ${filename}_comp.s16 --rateK --rateK_mean_min 10 --rateK_mean_max 40 --rateKin ${filename}_test.f32  \
-        --phase0 --postfilter -o - | sox -t .s16 -r 8000 -c 1 - ${o}/${filename}_comp_vq2.wav
-  cat ${filename}.f32 | \
-      vq_mbest --st $Kst --en $Ken -k $K -q vq_stage1.f32,vq_stage2.f32,vq_stage3.f32 --mbest 5 \
-      > ${filename}_test.f32
-  c2sim ${filename}_comp.s16 --rateK --rateK_mean_min 10 --rateK_mean_max 40 --rateKin ${filename}_test.f32  \
-        --phase0 --postfilter -o - | sox -t .s16 -r 8000 -c 1 - ${o}/${filename}_comp_vq3.wav
-  c2sim $fullfile --rateK --newamp1vq \
-         --postfilter_newamp1 --phase0 --postfilter -o - | sox -t .s16 -r 8000 -c 1 - ${o}/${filename}_newamp1.wav
-}
-
-# train - AGC and 200Hz HP filter, subset VQ
-function train_agc() {
-  fullfile=$TRAIN
-  filename=$(basename -- "$fullfile")
-  extension="${filename##*.}"
-  filename="${filename%.*}"
-
-  agc ${fullfile}
-  c2sim ${filename}_agc.s16 --rateK --rateK_mean_min 0 --rateK_mean_max 60 --rateKout ${filename}_agc.f32
-  vqtrain ${filename}_agc.f32 $K 2048 vq_stage1.f32 -s 1e-3 --st $Kst --en $Ken -r stage2_in.f32
-  vqtrain stage2_in.f32 $K 512 vq_stage2.f32 -s 1e-3 --st $Kst --en $Ken -r stage3_in.f32
-  vqtrain stage3_in.f32 $K 512 vq_stage3.f32 -s 1e-3 --st $Kst --en $Ken
-}
-
-# train - AGC and 200Hz HP filter, subset VQ
-function listen_agc() {
-  fullfile=$1
-  filename=$(basename -- "$fullfile")
-  extension="${filename##*.}"
-  filename="${filename%.*}"
-
-  o=agc
-  mkdir -p $o
-  agc ${fullfile}
-  c2sim ${filename}_agc.s16 --rateK --rateK_mean_min 0 --rateK_mean_max 60 --rateKout ${filename}.f32 \
-        --phase0 --postfilter --dump ${filename} -o - | sox -t .s16 -r 8000 -c 1 - ${o}/${filename}_ratek.wav
-  cat ${filename}.f32 | vq_mbest --st $Kst --en $Ken -k $K -q vq_stage1.f32 > ${filename}_vq.f32
-  c2sim ${filename}_agc.s16 --rateK --rateK_mean_min 0 --rateK_mean_max 60 --rateKin ${filename}_vq.f32  \
-        --phase0 --postfilter --postfilter_newamp1 -o - | sox -t .s16 -r 8000 -c 1 - ${o}/${filename}_agc_vq.wav
-  cat ${filename}.f32 | vq_mbest --st $Kst --en $Ken -k $K -q vq_stage1.f32,vq_stage2.f32 --mbest 5 \
-      > ${filename}_vq2.f32
-  c2sim ${filename}_agc.s16 --rateK --rateK_mean_min 0 --rateK_mean_max 60 --rateKin ${filename}_vq2.f32  \
-        --phase0 --postfilter --postfilter_newamp1 -o - | sox -t .s16 -r 8000 -c 1 - ${o}/${filename}_agc_vq2.wav
-  cat ${filename}.f32 | \
-      vq_mbest --st $Kst --en $Ken -k $K -q vq_stage1.f32,vq_stage2.f32,vq_stage3.f32 --mbest 5 \
-      > ${filename}_vq3.f32
-  c2sim ${filename}_agc.s16 --rateK --rateK_mean_min 0 --rateK_mean_max 60 --rateKin ${filename}_vq3.f32  \
-        --phase0 --postfilter --postfilter_newamp1 -o - | sox -t .s16 -r 8000 -c 1 - ${o}/${filename}_agc_vq3.wav
-  c2sim ${filename}_agc.s16 --rateK --rateK_mean_min 0 --rateK_mean_max 60 --rateKin ${filename}_vq3.f32  \
-         -o - | sox -t .s16 -r 8000 -c 1 - ${o}/${filename}_agc_vq3_op.wav
-  c2sim $fullfile --rateK --newamp1vq \
-         --phase0 --postfilter --postfilter_newamp1 -o - | sox -t .s16 -r 8000 -c 1 - ${o}/${filename}_newamp1b.wav
-}
-
-function listen_agc_nm() {
-  fullfile=$1
-  filename=$(basename -- "$fullfile")
-  extension="${filename##*.}"
-  filename="${filename%.*}"
-
-  o=agc_nm2
-  mkdir -p $o
-  agc ${fullfile}
-  c2sim ${filename}_agc.s16 --rateK --rateK_mean_min 0 --rateK_mean_max 60 --rateKnomeanout ${filename}.f32 \
-        --phase0 --postfilter --dump ${filename} -o - | sox -t .s16 -r 8000 -c 1 - ${o}/${filename}_ratek.wav
-  cat ${filename}.f32 | vq_mbest --st $Kst --en $Ken -k $K -q vq_stage1.f32 > ${filename}_vq.f32
-  c2sim ${filename}_agc.s16 --rateK --rateK_mean_min 0 --rateK_mean_max 60 --rateKnomeanin ${filename}_vq.f32  \
-        --phase0 --postfilter --postfilter_newamp1 -o - | sox -t .s16 -r 8000 -c 1 - ${o}/${filename}_agc_vq.wav
-  cat ${filename}.f32 | vq_mbest --st $Kst --en $Ken -k $K -q vq_stage1.f32,vq_stage2.f32 --mbest 5 \
-      > ${filename}_vq2.f32
-  c2sim ${filename}_agc.s16 --rateK --rateK_mean_min 0 --rateK_mean_max 60 --rateKnomeanin ${filename}_vq2.f32  \
-        --phase0 --postfilter --postfilter_newamp1 -o - | sox -t .s16 -r 8000 -c 1 - ${o}/${filename}_agc_vq2.wav
-  cat ${filename}.f32 | \
-      vq_mbest --st $Kst --en $Ken -k $K -q vq_stage1.f32,vq_stage2.f32,vq_stage3.f32 --mbest 5 \
-      > ${filename}_vq3.f32
-  c2sim ${filename}_agc.s16 --rateK --rateK_mean_min 0 --rateK_mean_max 60 --rateKnomeanin ${filename}_vq3.f32  \
-        --phase0 --postfilter --postfilter_newamp1 -o - | sox -t .s16 -r 8000 -c 1 - ${o}/${filename}_agc_vq3.wav
-  c2sim ${filename}_agc.s16 --rateK --rateK_mean_min 0 --rateK_mean_max 60 --rateKnomeanin ${filename}_vq3.f32  \
-         -o - | sox -t .s16 -r 8000 -c 1 - ${o}/${filename}_agc_vq3_op.wav
-  c2sim $fullfile --rateK --newamp1vq \
-         --phase0 --postfilter --postfilter_newamp1 -o - | sox -t .s16 -r 8000 -c 1 - ${o}/${filename}_newamp1b.wav
-}
-
-function train_agc_nm() {
-  fullfile=$TRAIN
-  filename=$(basename -- "$fullfile")
-  extension="${filename##*.}"
-  filename="${filename%.*}"
-
-  agc ${fullfile}
-  c2sim ${filename}_agc.s16 --rateK --rateKout ${filename}_agc.f32
-  extract ${filename}_agc.f32 ${filename}_agc_nm.f32 -t $K -s $Kst -e $Ken --writeall --lower 10
-  vqtrain ${filename}_agc_nm.f32 $K 2048 vq_stage1.f32 -s 1e-3 --st $Kst --en $Ken -r stage2_in.f32
-  vqtrain stage2_in.f32 $K 512 vq_stage2.f32 -s 1e-3 --st $Kst --en $Ken -r stage3_in.f32
-  vqtrain stage3_in.f32 $K 512 vq_stage3.f32 -s 1e-3 --st $Kst --en $Ken
-}
-
-# remove mean based on mean of Kst to Ken
 function train_agc_nm2() {
   fullfile=$TRAIN
   filename=$(basename -- "$fullfile")
@@ -183,6 +47,58 @@ function train_agc_nm2() {
   c2sim ${filename}_agc.s16 --rateK --rateKout ${filename}_agc.f32
   extract ${filename}_agc.f32 ${filename}_agc_nm.f32 -t $K -s $Kst -e $Ken --removemean --writeall --lower 10
   vqtrain ${filename}_agc_nm.f32 $K 2048 vq_stage1.f32 -s 1e-3 --st $Kst --en $Ken -r stage2_in.f32
+  vqtrain stage2_in.f32 $K 512 vq_stage2.f32 -s 1e-3 --st $Kst --en $Ken -r stage3_in.f32
+  vqtrain stage3_in.f32 $K 512 vq_stage3.f32 -s 1e-3 --st $Kst --en $Ken
+}
+
+function listen_agc_nm() {
+  fullfile=$1
+  filename=$(basename -- "$fullfile")
+  extension="${filename##*.}"
+  filename="${filename%.*}"
+
+  o=agc_nm3
+  mkdir -p $o
+  agc ${fullfile}
+  c2sim ${filename}_agc.s16 --rateK --rateK_mean_min 0 --rateK_mean_max 60 --rateKnomeanout ${filename}.f32 \
+        --K_st  $Kst --K_en $Ken \
+        --phase0 --postfilter --dump ${filename} -o - | sox -t .s16 -r 8000 -c 1 - ${o}/${filename}_ratek.wav
+  cat ${filename}.f32 | vq_mbest --st $Kst --en $Ken -k $K -q vq_stage1.f32 > ${filename}_vq.f32
+  c2sim ${filename}_agc.s16 --rateK --rateK_mean_min 0 --rateK_mean_max 60 --rateKnomeanin ${filename}_vq.f32  \
+        --K_st $Kst  --K_en $Ken \
+        --phase0 --postfilter --postfilter_newamp1 -o - | sox -t .s16 -r 8000 -c 1 - ${o}/${filename}_agc_vq.wav
+  cat ${filename}.f32 | vq_mbest --st $Kst --en $Ken -k $K -q vq_stage1.f32,vq_stage2.f32 --mbest 5 \
+      > ${filename}_vq2.f32
+  c2sim ${filename}_agc.s16 --rateK --rateK_mean_min 0 --rateK_mean_max 60 --rateKnomeanin ${filename}_vq2.f32  \
+        --K_st $Kst --K_en $Ken \
+        --phase0 --postfilter --postfilter_newamp1 -o - | sox -t .s16 -r 8000 -c 1 - ${o}/${filename}_agc_vq2.wav
+  cat ${filename}.f32 | \
+      vq_mbest --st $Kst --en $Ken -k $K -q vq_stage1.f32,vq_stage2.f32,vq_stage3.f32 --mbest 5 \
+      > ${filename}_vq3.f32
+  c2sim ${filename}_agc.s16 --rateK --rateK_mean_min 0 --rateK_mean_max 60 --rateKnomeanin ${filename}_vq3.f32  \
+        --K_st $Kst --K_en $Ken \
+        --phase0 --postfilter --postfilter_newamp1 -o - | sox -t .s16 -r 8000 -c 1 - ${o}/${filename}_agc_vq3.wav
+  c2sim ${filename}_agc.s16 --rateK --rateK_mean_min 0 --rateK_mean_max 60 --rateKnomeanin ${filename}_vq3.f32  \
+        --K_st $Kst --K_en $Ken \
+         -o - | sox -t .s16 -r 8000 -c 1 - ${o}/${filename}_agc_vq3_op.wav
+  c2sim $fullfile --rateK --newamp1vq \
+         --phase0 --postfilter --postfilter_newamp1 -o - | sox -t .s16 -r 8000 -c 1 - ${o}/${filename}_newamp1b.wav
+}
+
+# ---------------------------------------------------------------------------------
+# AGC, two band freq domain Hilbert compressor
+# ---------------------------------------------------------------------------------
+
+function train_compressed_two_band() {
+  fullfile=$TRAIN
+  filename=$(basename -- "$fullfile")
+  extension="${filename##*.}"
+  filename="${filename%.*}"
+
+  agc ${fullfile}
+  c2sim ${filename}_agc.s16 --rateK --rateKout ${filename}_comp.f32 --comp 75 --comp_gain 10
+  extract ${filename}_comp.f32 ${filename}_comp_nm.f32 -t $K -s $Kst -e $Ken --writeall --lower 20 --removemean
+  vqtrain ${filename}_comp_nm.f32 $K 2048 vq_stage1.f32 -s 1e-3 --st $Kst --en $Ken -r stage2_in.f32
   vqtrain stage2_in.f32 $K 512 vq_stage2.f32 -s 1e-3 --st $Kst --en $Ken -r stage3_in.f32
   vqtrain stage3_in.f32 $K 512 vq_stage3.f32 -s 1e-3 --st $Kst --en $Ken
 }
@@ -198,38 +114,29 @@ function listen_agc_nm_comp() {
   mkdir -p $o
   agc ${fullfile}
   c2sim ${filename}_agc.s16 --rateK --comp 75 --comp_gain 10 --rateKnomeanout ${filename}.f32 \
+        --K_st $Kst --K_en $Ken \
         --phase0 --postfilter --dump ${filename} -o - | sox -t .s16 -r 8000 -c 1 - ${o}/${filename}_ratek.wav
   cat ${filename}.f32 | vq_mbest --st $Kst --en $Ken -k $K -q vq_stage1.f32 > ${filename}_vq.f32
   c2sim ${filename}_agc.s16 --rateK --comp 75 --comp_gain 10 --rateKnomeanin ${filename}_vq.f32  \
+        --K_st $Kst --K_en $Ken \
         --phase0 --postfilter --postfilter_newamp1 -o - | sox -t .s16 -r 8000 -c 1 - ${o}/${filename}_agc_vq.wav
   cat ${filename}.f32 | vq_mbest --st $Kst --en $Ken -k $K -q vq_stage1.f32,vq_stage2.f32 --mbest 5 \
       > ${filename}_vq2.f32
   c2sim ${filename}_agc.s16 --rateK --comp 75 --comp_gain 10 --rateKnomeanin ${filename}_vq2.f32  \
+        --K_st $Kst --K_en $Ken \
         --phase0 --postfilter --postfilter_newamp1 -o - | sox -t .s16 -r 8000 -c 1 - ${o}/${filename}_agc_vq2.wav
   cat ${filename}.f32 | \
       vq_mbest --st $Kst --en $Ken -k $K -q vq_stage1.f32,vq_stage2.f32,vq_stage3.f32 --mbest 5 \
       > ${filename}_vq3.f32
   c2sim ${filename}_agc.s16 --rateK --comp 75 --comp_gain 10 --rateKnomeanin ${filename}_vq3.f32  \
+        --K_st $Kst --K_en $Ken \
         --phase0 --postfilter --postfilter_newamp1 -o - | sox -t .s16 -r 8000 -c 1 - ${o}/${filename}_agc_vq3.wav
   c2sim ${filename}_agc.s16 --rateK --comp 75 --comp_gain 10 --rateKnomeanin ${filename}_vq3.f32  \
-         -o - | sox -t .s16 -r 8000 -c 1 - ${o}/${filename}_agc_vq3_op.wav
+        --K_st $Kst --K_en $Ken \
+        -o - | sox -t .s16 -r 8000 -c 1 - ${o}/${filename}_agc_vq3_op.wav
   c2sim $fullfile --rateK --newamp1vq \
-         --phase0 --postfilter --postfilter_newamp1 -o - | sox -t .s16 -r 8000 -c 1 - ${o}/${filename}_newamp1.wav
-}
-
-# train - two band freq domain Hilbert compressor
-function train_compressed_two_band() {
-  fullfile=$TRAIN
-  filename=$(basename -- "$fullfile")
-  extension="${filename##*.}"
-  filename="${filename%.*}"
-
-  agc ${fullfile}
-  c2sim ${filename}_agc.s16 --rateK --rateKnomeanout ${filename}_comp.f32 --comp 75 --comp_gain 10
-  #extract ${filename}_comp.f32 ${filename}_comp_nm.f32 -t $K -s $Kst -e $Ken --writeall --lower 20 --removemean
-  vqtrain ${filename}_comp.f32 $K 2048 vq_stage1.f32 -s 1e-3 --st $Kst --en $Ken -r stage2_in.f32
-  vqtrain stage2_in.f32 $K 512 vq_stage2.f32 -s 1e-3 --st $Kst --en $Ken -r stage3_in.f32
-  vqtrain stage3_in.f32 $K 512 vq_stage3.f32 -s 1e-3 --st $Kst --en $Ken
+        --K_st $Kst --K_en $Ken \
+        --phase0 --postfilter --postfilter_newamp1 -o - | sox -t .s16 -r 8000 -c 1 - ${o}/${filename}_newamp1.wav
 }
 
 # Look at some stats when running two band freq domain Hilbert compressor
@@ -252,9 +159,12 @@ function test_agc_nm2() {
   cat ${filename}_agc_nm.f32 | vq_mbest --st $Kst --en $Ken -k $K -q vq_gain.f32,vq_stage1.f32,vq_stage2.f32,vq_stage3.f32 --mbest 13 > /dev/null      
 }
 
+# -----------------------------------------------------------------------
 # Exploring the effect of the postfilter with newamp1 and phase0
 #  + listened with headphones
 #  + PF improves muffled/clicky but adds tonal artefact and AM modulation
+# -----------------------------------------------------------------------
+
 function listen_newamp1() {
   fullfile=$1
   filename=$(basename -- "$fullfile")
@@ -275,24 +185,6 @@ function listen_newamp1() {
         | sox -t .s16 -r 8000 -c 1 - ${o}/${filename}_newamp1_p0_pf.wav
 }
 
-function comp_test() {
-    compress $CODEC2_PATH/raw/vk5qi.raw
-    compress $CODEC2_PATH/raw/kristoff.raw
-    compress $CODEC2_PATH/raw/big_dog.raw
-    compress $CODEC2_PATH/raw/hts2a.raw
-    compress ~/Downloads/fish.s16
-    compress ~/Downloads/pencil.s16
-}
-
-function listen_test_compressed() {
-    listen_compressed $CODEC2_PATH/raw/big_dog.raw
-    listen_compressed $CODEC2_PATH/raw/hts2a.raw
-    listen_compressed ~/Downloads/fish.s16
-    listen_compressed ~/Downloads/pencil.s16
-    listen_compressed $CODEC2_PATH/raw/kristoff.raw
-    listen_compressed $CODEC2_PATH/raw/vk5qi.raw
-    listen_compressed ~/Downloads/vk5dgr_testing_8k.wav
-}
 function listen_test_newamp1() {
     listen_newamp1 $CODEC2_PATH/raw/big_dog.raw
     listen_newamp1 $CODEC2_PATH/raw/hts2a.raw
@@ -300,16 +192,6 @@ function listen_test_newamp1() {
     listen_newamp1 ~/Downloads/pencil.s16
     listen_newamp1 $CODEC2_PATH/raw/kristoff.raw
     listen_newamp1 $CODEC2_PATH/raw/vk5qi.raw
-}
-
-function listen_test_agc() {
-    listen_agc $CODEC2_PATH/raw/big_dog.raw
-    listen_agc $CODEC2_PATH/raw/hts2a.raw
-    listen_agc ~/Downloads/fish.s16
-    listen_agc ~/Downloads/pencil.s16
-    listen_agc $CODEC2_PATH/raw/kristoff.raw
-    listen_agc $CODEC2_PATH/raw/vk5qi.raw
-    listen_agc ~/Downloads/vk5dgr_testing_8k.wav
 }
 
 function listen_test_agc_nm() {
@@ -332,16 +214,9 @@ function listen_test_agc_nm_comp() {
     listen_agc_nm_comp ~/Downloads/vk5dgr_testing_8k.wav
 }
 
-#train_compressed
-#listen_test_compressed
-#train_agc
-#listen_test_agc
-#train_agc_nm
-#train_agc_nm
-train_agc_nm2
-listen_test_agc_nm
-#test_agc_nm2
-#stats_compressed_two_band
+
 #train_compressed_two_band
 #listen_test_agc_nm_comp
-#listen_agc_nm_comp ~/Downloads/fish.s16
+
+train_agc_nm2
+listen_test_agc_nm
