0fdd95c76120dad42d56641030abb2edb23db0e1
David Rowe <david@rowetel.com>
2022-07-24 15:21:07 +0930

    now codec2_math.h is being included, we need comp.h (I think)

--- src/codec2_math.h
+++ src/codec2_math.h
@@ -31,8 +31,7 @@ extern "C"
 {
 #endif // __cplusplus
 
-// Forward declare COMP struct (use comp.h to refer to items inside the struct).
-struct COMP;
+#include "comp.h"
 
 //==========================================================================
 // Note: the functions in this file must be implemented in your code if you 
@@ -50,7 +49,7 @@ struct COMP;
 /// @param resultReal A pointer to the variable in which to store the scalar result.
 ///
 //==========================================================================
-extern void codec2_dot_product_f32(float* leftHandSideComplexVector, float* rightHandSideComplexVector, size_t vectorLength, float* resultReal) __attribute__((weak));
+void codec2_dot_product_f32(float* leftHandSideComplexVector, float* rightHandSideComplexVector, size_t vectorLength, float* resultReal);
 
 //==========================================================================
 /// Calculates the dot product of two complex-valued float vectors.
@@ -62,7 +61,7 @@ extern void codec2_dot_product_f32(float* leftHandSideComplexVector, float* righ
 /// @param resultImag A pointer to the variable in which to store the imaginary component of the result.
 ///
 //==========================================================================
-extern void codec2_complex_dot_product_f32(COMP* leftHandSideComplexVector, COMP* rightHandSideComplexVector, size_t vectorLength, float* resultReal, float* resultImag) __attribute__((weak));
+void codec2_complex_dot_product_f32(COMP* leftHandSideComplexVector, COMP* rightHandSideComplexVector, size_t vectorLength, float* resultReal, float* resultImag);
 
 #ifdef __cplusplus
 }
