f49c5f1c3f9163b586edb01f6938598c164b35e9
drowe67 <david@rowetel.com>
2022-12-01 14:51:06 +1030

    plotting SNR est versus measured curves for 3 raw data modes

--- octave/snr_curves_plot.m
+++ octave/snr_curves_plot.m
@@ -6,16 +6,24 @@
 
 function snr_scatter(mode, fmt)
   snrch = load(sprintf("../unittest/snrch_%s.txt",mode));
+  snroffset = load(sprintf("../unittest/snroffset_%s.txt",mode));
+  snrch -= snroffset;
   for i=1:length(snrch)
     fn = sprintf('../unittest/snrest_%s_%d.txt',mode,i);
     if exist(fn,'file') == 2
       snrest=load(fn);
-      plot(snrch(i)*ones(1,length(snrest)), snrest,fmt);
+      if i == 1
+        plot(snrch(i)*ones(1,length(snrest)), snrest, sprintf('%s;%s;',fmt,mode));
+      else
+        plot(snrch(i)*ones(1,length(snrest)), snrest, fmt);
+     end
     end
   end
 endfunction
 
 figure(1); clf; hold on;
-snr_scatter('datac3','b+')
+snr_scatter('datac0','b+')
+snr_scatter('datac1','g+')
+snr_scatter('datac3','r+')
 xlabel('SNR (dB)'); ylabel('SNRest (dB)')
-legend({'datac3'})
+%legend({'datac0','datac3'})
--- unittest/snr_curves.sh
+++ unittest/snr_curves.sh
@@ -13,28 +13,38 @@ CODEC2=${HOME}/codec2
 # run all three modes
 # run AWGN + MPP channels
 
-No_list='-18 -16 -14 -12 -10'
+No_list='-20 -18 -16 -14 -12 -10'
 Ntestframes=10
 mode='datac3'
 
-ch_log=$(mktemp)
-rx_log=$(mktemp)
+function generate_curve_data {
+  mode=$1
+  tx_log=$(mktemp)
+  ch_log=$(mktemp)
+  rx_log=$(mktemp)
 
-i=1
-rm snrest_${mode}*.txt
-for No in $No_list
-do
-  freedv_data_raw_tx --bursts $Ntestframes --testframes $Ntestframes $mode /dev/zero - 2</dev/null| \
-  ch - - --No $No -f 20 2>>${ch_log} | \
-  freedv_data_raw_rx --testframes $mode - /dev/null 2>${rx_log} -v
-  SNRest=$(cat ${rx_log} | grep '\-BS\-' | tr -s ' ' | cut -d' ' -f17)
-  if [ ! -z "$SNRest" ]; then
-    echo ${SNRest} > snrest_${mode}_${i}.txt
-  fi
-  PERmeas=$(cat ${rx_log} | grep 'Coded FER' | cut -d' ' -f3)
-  echo ${PERest} >> per_${mode}.txt
-  i=$((i+1))
-done
+  i=1
+  rm snrest_${mode}*.txt
+  for No in $No_list
+  do
+    freedv_data_raw_tx --bursts $Ntestframes --testframes $Ntestframes $mode /dev/zero - 2>${tx_log} | \
+    ch - - --No $No -f 20 2>>${ch_log} | \
+    freedv_data_raw_rx --testframes $mode - /dev/null 2>${rx_log} -v
+    SNRoffset=$(cat ${tx_log} | grep "mark:space" | tr -s ' ' | cut -d' ' -f 5)
+    echo ${SNRoffset} > snroffset_${mode}.txt
+    SNRest=$(cat ${rx_log} | grep '\-BS\-' | tr -s ' ' | cut -d' ' -f17)
+    if [ ! -z "$SNRest" ]; then
+      echo ${SNRest} > snrest_${mode}_${i}.txt
+    fi
+    PERmeas=$(cat ${rx_log} | grep 'Coded FER' | cut -d' ' -f3)
+    echo ${PERest} >> per_${mode}.txt
+    i=$((i+1))
+  done
 
-SNRch=$(cat ${ch_log} | grep SNR3k | tr -s ' ' | cut -d' ' -f3)
-echo ${SNRch} > snrch_${mode}.txt
+  SNRch=$(cat ${ch_log} | grep SNR3k | tr -s ' ' | cut -d' ' -f3)
+  echo ${SNRch} > snrch_${mode}.txt
+}
+
+generate_curve_data 'datac0'
+generate_curve_data 'datac1'
+generate_curve_data 'datac3'
