a4c6f781477db709b9423a8bbce46a95e97b18ec
drowe67 <david@rowetel.com>
2022-12-01 13:25:58 +1030

    building up snr_curves.sh

---
+++ unittest/snr_curves.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+# snr_curves.sh
+#
+# Plot SNR curves for raw data modes
+
+set -x
+
+PATH=${PATH}:${HOME}/codec2/build_linux/src
+CODEC2=${HOME}/codec2
+
+# run over a range of No values
+# pull our SNR meas, SNR est, PER
+# run all three modes
+# run AWGN + MPP channels
+
+No_list='-18 -16 -14 -12 -10'
+Ntestframes=10
+mode='datac3'
+
+ch_log=$(mktemp)
+rx_log=$(mktemp)
+
+i=1
+for No in $No_list
+do
+  freedv_data_raw_tx --bursts $Ntestframes --testframes $Ntestframes $mode /dev/zero - 2</dev/null| \
+  ch - - --No $No -f 20 2>>${ch_log} | \
+  freedv_data_raw_rx --testframes $mode - /dev/null 2>${rx_log} -v
+  SNRest=$(cat ${rx_log} | grep '\-BS\-' | tr -s ' ' | cut -d' ' -f17)
+  echo ${SNRest} > snrest${i}.txt
+  PERmeas=$(cat ${rx_log} | grep 'Coded FER' | cut -d' ' -f3)
+  echo ${PERest} >> per.txt
+  i=$((i+1))
+done
+
+SNRch=$(cat ${ch_log} | grep SNR3k | tr -s ' ' | cut -d' ' -f3)
+echo ${SNRch} > snrch.txt
