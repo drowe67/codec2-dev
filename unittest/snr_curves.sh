1fbdd118a59be849c72bd4eba0a658a3761417e2
drowe67 <david@rowetel.com>
2022-12-04 10:18:38 +1030

    Octave Tx PER/BER versus SNR

--- unittest/snr_curves.sh
+++ unittest/snr_curves.sh
@@ -1,25 +1,58 @@
 #!/usr/bin/env bash
 # snr_curves.sh
 #
-# Plot SNR curves for raw data modes
+# The raw data mode modems can estimate channel SNR from the Rx signal. This
+# script (and companion Octave snr_curves_plot.m) plots estimated versus actual
+# SNR curves for raw data modes
 
 set -x
 
 PATH=${PATH}:${HOME}/codec2/build_linux/src
 CODEC2=${HOME}/codec2
 
-# run over a range of No values
-# pull our SNR meas, SNR est, PER
-# run all three modes
-# run AWGN + MPP channels
+# 1. Use Octave Tx built-in channel simulation as source of truth for actual SNR.
+# 2. Check the C "ch" tool calibration against Octave source of truth.
+# 3. Plot SNR est versus SNR for clean and clipped waveforms
+# 4. Plot PER versus SNR
+# 5. Further work: plot PER versus peak power to show benefit of clipping, 
+#                  extend to MPP channels
 
+snr_list='-4 -2 0 2 4'
 No_list='-28 -26 -24 -22 -20 -18 -16 -14 -12 -10 -8'
-Ntestframes=25
-mode='datac3'
+Nbursts=10
 
-function generate_curve_data {
+# Using Octave Tx as source of truth for SNR, generate BER/PER v SNR
+function generate_octave_tx_data {
   mode=$1
-  clip=$2
+
+  tx_log=$(mktemp)
+  ch_log=$(mktemp)
+  rx_log=$(mktemp)
+
+  i=1
+  rm snr_oct_${mode}*.txt
+  rm ber_oct_${mode}*.txt
+  rm per_oct_${mode}*.txt
+  for snr in $snr_list
+  do
+    echo "ofdm_ldpc_tx('test_${mode}.raw','${mode}',1,${snr},'awgn','bursts',10,'crc'); quit" | \
+    DISPLAY="" octave-cli -p ${CODEC2}/octave
+    freedv_data_raw_rx --testframes $mode test_${mode}.raw /dev/null 2>${rx_log} -v
+    cat ${rx_log}
+    BERmeas=$(cat ${rx_log} | grep 'BER......:' | cut -d' ' -f2)
+    PERmeas=$(cat ${rx_log} | grep 'Coded FER' | cut -d' ' -f3)
+    
+    echo ${snr} >> snr_oct_${mode}.txt
+    echo ${BERmeas} >> ber_oct_${mode}.txt
+    echo ${PERmeas} >> per_oct_${mode}.txt
+    i=$((i+1))
+  done
+}
+
+# Using ch as source of truth for channel SNR, collect SNR estimates from modem
+function generate_snrest_v_snr_data {
+  mode=$1
+
   tx_log=$(mktemp)
   ch_log=$(mktemp)
   rx_log=$(mktemp)
@@ -29,7 +62,7 @@ function generate_curve_data {
   rm per_${mode}*.txt
   for No in $No_list
   do
-    freedv_data_raw_tx --clip $clip --bursts $Ntestframes --testframes $Ntestframes $mode /dev/zero - 2>${tx_log} | \
+    freedv_data_raw_tx --bursts $Nbursts --testframes $Nbursts $mode /dev/zero - 2>${tx_log} | \
     ch - - --No $No -f 20 2>>${ch_log} | \
     freedv_data_raw_rx --testframes $mode - /dev/null 2>${rx_log} -v
     SNRoffset=$(cat ${tx_log} | grep "mark:space" | tr -s ' ' | cut -d' ' -f 5)
@@ -47,6 +80,8 @@ function generate_curve_data {
   echo ${SNRch} > snrch_${mode}.txt
 }
 
-generate_curve_data 'datac0' 1
-generate_curve_data 'datac1' 0
-generate_curve_data 'datac3' 1
+generate_octave_tx_data 'datac0'
+
+#generate_snrest_v_snr_data 'datac0'
+#generate_snrest_v_snr_data 'datac1'
+#generate_snrest_v_snr_data 'datac3'
