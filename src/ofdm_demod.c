3d03d2b8bca59772e5f58f2e3a7cb44693f3045d
Mooneer Salem <mooneer@gmail.com>
2022-12-31 01:57:13 -0800

    ofdm_destroy() should be done after we're finished with the ofdm object.

--- src/ofdm_demod.c
+++ src/ofdm_demod.c
@@ -634,8 +634,6 @@ int main(int argc, char *argv[]) {
         f++;
     }
 
-    ofdm_destroy(ofdm);
-
     if (input_specified == true)
         fclose(fin);
 
@@ -695,5 +693,7 @@ int main(int argc, char *argv[]) {
         fprintf(stderr, "Npre.....: %6d Npost: %5d uw_fails: %2d\n", ofdm->pre, ofdm->post, ofdm->uw_fails);
     }
     
+    ofdm_destroy(ofdm);
+
     return ret;
 }
