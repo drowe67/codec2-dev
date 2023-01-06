3a93a28477a6f13c2ecedd30c8d6729cb157c1e4
drowe67 <david@rowetel.com>
2023-01-06 17:44:03 +1030

    dynamic range clamp feature

--- misc/extract.c
+++ misc/extract.c
@@ -2,7 +2,7 @@
   extract.c
   david Rowe Jan 2019
 
-  Extracts sub sets of vectors from .f32 files, used for LPCNet VQ experiments.
+  Vector Quantiser tool that pre-processes training data before vq_train.
 */
 
 #include <assert.h>
@@ -24,24 +24,26 @@ int main(int argc, char *argv[]) {
     int removemean = 0;
     float lower = -1E32;
     int writeall = 0;
+    float dynamicrange = 0.0;
     
     static struct option long_options[] = {
-        {"startcol",   required_argument, 0, 's'},
-        {"endcol",     required_argument, 0, 'e'},
-        {"stride",     required_argument, 0, 't'},
-        {"gain",       required_argument, 0, 'g'},
-        {"pred",       required_argument, 0, 'p'},
-        {"delay",      required_argument, 0, 'd'},
-        {"removemean", no_argument, 0, 'm'},
-        {"lower",      required_argument, 0, 'l'},
-        {"writeall", no_argument, 0, 'w'},
+        {"startcol",      required_argument, 0, 's'},
+        {"endcol",        required_argument, 0, 'e'},
+        {"stride",        required_argument, 0, 't'},
+        {"gain",          required_argument, 0, 'g'},
+        {"pred",          required_argument, 0, 'p'},
+        {"delay",         required_argument, 0, 'd'},
+        {"removemean",    no_argument,       0, 'm'},
+        {"lower",         required_argument, 0, 'l'},
+        {"dynamicrange", required_argument, 0, 'y'},
+        {"writeall",      no_argument,       0, 'w'},
         {0, 0, 0, 0}
     };
 
     int opt_index = 0;
     int c;
     
-    while ((c = getopt_long (argc, argv, "s:e:t:g:p:d:ml:", long_options, &opt_index)) != -1) {
+    while ((c = getopt_long (argc, argv, "s:e:t:g:p:d:ml:y:", long_options, &opt_index)) != -1) {
         switch (c) {
         case 's':
             st = atoi(optarg);
@@ -70,10 +72,22 @@ int main(int argc, char *argv[]) {
         case 'w':
             writeall = 1;
             break;
+        case 'y':
+            dynamicrange = atof(optarg);
+            break;
         default:
         helpmsg:
-            fprintf(stderr, "usage: %s  -s startCol -e endCol [-t strideCol -g gain -p predCoeff -d "
-                            "framesDelay --removemean --lower --writeall] input.f32 output.f32\n", argv[0]);
+            fprintf(stderr, "usage: %s  -s startCol -e endCol [options] input.f32 output.f32\n"
+                            "\n"
+                            "-t strideCol           Vector dimenstion, e.g. 30\n"
+                            "-g gain                Gain applied to vectors\n"
+                            "-p predCoeff           Coefficient used for prediction, e.g. 0.9\n"
+                            "-d framesDelay Delay   Delay (frames) between vectors used for prediction\n"
+                            "--removemean           Remove mean from vectors\n"
+                            "--lower minEnergy      Remove all vectors less than minEnergy\n"
+                            "--writeall             Write all K outputs for each vectoir, even if EndCol-StartCol+1 < stride\n"
+                            "--dynamicrange RangedB Clip min value of each vector to max - RangedB\n"
+                            "input.f32 output.f32\n", argv[0]);
             exit(1);
         }
     }
@@ -82,10 +96,16 @@ int main(int argc, char *argv[]) {
         goto helpmsg;
     }
  
+    if ((lower != -1E32) && (pred != 0.0)) {
+        /* These two options cause unexpected jumps in predictive VQ training data */ 
+        fprintf(stderr, "Warning - low energy vector removal and prediction enabled\n");
+    }
+    
     fin = fopen(argv[optind],"rb"); assert(fin != NULL);
     fout = fopen(argv[optind+1],"wb"); assert(fout != NULL);
-    printf("extracting from %d to %d inclusive (stride %d) ... gain = %f pred = %f frame_delay = %d\n",
-           st, en, stride, gain, pred, frame_delay);
+    printf("extracting from %d to %d inclusive (stride %d) ... \n"
+           "gain = %f pred = %f frame_delay = %d dynamic range = %f\n",
+           st, en, stride, gain, pred, frame_delay, dynamicrange);
    
     float features[stride], features_prev[frame_delay][stride], delta[stride];
     int i,f,rd=0,wr=0;
@@ -97,6 +117,16 @@ int main(int argc, char *argv[]) {
             features_prev[f][i] = 0.0;
 
     while((fread(features, sizeof(float), stride, fin) == stride)) {
+        if (dynamicrange != 0.0) {
+            float max = -1E32;
+	    for(i=st; i<=en; i++)
+	        if (features[i] > max) max = features[i];
+	    for(i=st; i<=en; i++) {
+                float min = max-dynamicrange;
+                if (features[i] < min ) features[i] = min;
+            }        
+        }
+        
 	float mean = 0.0;
 	for(i=st; i<=en; i++)
 	    mean += features[i];
@@ -105,6 +135,7 @@ int main(int argc, char *argv[]) {
 	    for(i=0; i<stride; i++)
 		features[i] -= mean;
 	}
+        
 	for(i=st; i<=en; i++) {
 	    delta[i] = gain*(features[i] - pred*features_prev[frame_delay-1][i]);
 	}
