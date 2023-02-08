ec1c1830e5821209a9057f79edde67930cd2abe1
drowe67 <david@rowetel.com>
2023-02-08 17:37:12 +1030

    added pred2 feeature, extract x[n-1]-(x[n]+x[n-3])/2

--- misc/extract.c
+++ misc/extract.c
@@ -42,6 +42,7 @@ int main(int argc, char *argv[]) {
     int timestep = 1;
     int timeoffset = 0;
     int mean_l2 = 0;
+    int pred2 = 0;
     
     static struct option long_options[] = {
         {"startcol",      required_argument, 0, 's'},
@@ -58,6 +59,7 @@ int main(int argc, char *argv[]) {
         {"dynamicrange",  required_argument, 0, 'y'},
         {"drlate",        no_argument,       0, 'z'},
         {"writeall",      no_argument,       0, 'w'},
+        {"pred2",         no_argument,       0, 'r'},
         {0, 0, 0, 0}
     };
 
@@ -110,6 +112,9 @@ int main(int argc, char *argv[]) {
         case 'z':
             drlate = 1;
             break;
+        case 'r':
+            pred2 = 1;
+            break;
         default:
         helpmsg:
             fprintf(stderr, "VQ pre-processing tool\n\nusage: %s  -s startCol -e endCol [options] input.f32 output.f32\n"
@@ -118,6 +123,7 @@ int main(int argc, char *argv[]) {
                             "-g gain                Gain applied to vectors\n"
                             "-p predCoeff           Coefficient used for prediction, e.g. 0.9\n"
                             "-d predDelay           Delay (frames) between vectors used for prediction\n"
+                            "--pred2                Output x[n-1]-(x[n]+x[n-3])/2, use with -d 4\n"
                             "--removemean           Remove mean from vectors\n"
                             "--lower minEnergy      Remove all vectors less than minEnergy\n"
                             "--writeall             Write all K outputs for each vector, even if EndCol-StartCol+1 < stride\n"
@@ -134,7 +140,7 @@ int main(int argc, char *argv[]) {
         goto helpmsg;
     }
  
-    if ((lower != -1E32) && (pred != 0.0)) {
+    if (removemean && (pred != 0.0)) {
         /* These two options cause unexpected jumps in predictive VQ training data */ 
         fprintf(stderr, "Warning - low energy vector removal and prediction enabled\n");
     }
@@ -145,11 +151,11 @@ int main(int argc, char *argv[]) {
            "gain = %f pred = %f pred_frame_delay = %d dynamic range = %f drlate = %d\n",
            st, en, stride, timestep, gain, pred, frame_delay, dynamicrange, drlate);
    
-    float features[stride], features_prev[frame_delay][stride], delta[stride];
+    float features[stride], features_prev[frame_delay][stride], out[stride];
     int i,f,rd=0,wr=0;
     
     for(i=0; i<stride; i++)
-        delta[i] = 0.0;
+        out[i] = 0.0;
     for (f=0; f<frame_delay; f++)
         for(i=0; i<stride; i++)
             features_prev[f][i] = 0.0;
@@ -180,18 +186,26 @@ int main(int argc, char *argv[]) {
 		features[i] -= mean;
 	}
         
-	for(i=st; i<=en; i++) {
-	    delta[i] = gain*(features[i] - pred*features_prev[frame_delay-1][i]);
-	}
+	if (pred2) {
+            /* prediction error from mean of past and future */
+            assert(frame_delay == 4);
+            for(i=st; i<=en; i++)
+                out[i] = features_prev[1][i] - 0.5*(features[i] + features_prev[3][i]);
+        }
+        else {
+            /* no prediction with pred == 0 */
+            for(i=st; i<=en; i++)
+                out[i] = gain*(features[i] - pred*features_prev[frame_delay-1][i]);
+        }
         
         if (drlate)
           restrict_dynamic_range(features, st, en, dynamicrange);
 
 	if (mean > lower) {
 	    if (writeall)
-                fwrite(delta, sizeof(float), stride, fout);
+                fwrite(out, sizeof(float), stride, fout);
             else
-                fwrite(&delta[st], sizeof(float), en-st+1, fout);
+                fwrite(&out[st], sizeof(float), en-st+1, fout);
 	    wr++;
 	}
 	for (f=frame_delay-1; f>0; f--)
